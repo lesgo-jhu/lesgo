0acd37e6932751b8c275fdc01d6cbb3197e17ca9
(no author) <(no author)@4c8967c9-1b3f-4110-b94f-c20ed91a0040>
2009-05-22 14:36:06 +0000

    This commit was manufactured by cvs2svn to create tag 'poly_root_method'.

--- compile_cyl_skew.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash 
-rm -v cylinder_skew
-#gfortran -I/usr/local/lib64 -fdefault-real-8 -fdefault-double-8 -g -c cylinder_skew.f90
-#gfortran -L/usr/local/lib -L/usr/local/lib64 -I/usr/local/lib64 -fdefault-real-8 -fdefault-double-8 -g -o cylinder_skew cylinder_skew.o -lgeometry -lafnl
-
-ifort -I/usr/local/lib64 -r8 -g -c cylinder_skew.f90
-ifort -L/usr/local/lib -L/usr/local/lib64 -I/usr/local/lib64 -r8 -g -o cylinder_skew cylinder_skew.o -lgeometry -lafnl
-
-
