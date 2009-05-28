897f189c5250059ae8bfec49a8b39c6b66f994a9
jgraham <jgraham@4c8967c9-1b3f-4110-b94f-c20ed91a0040>
2009-05-28 02:16:31 +0000

    Added defs from trunk; did not get transferred for some reason during the merge

---
+++ defs.f90
@@ -0,0 +1,94 @@
+module stat_defs
+!  Define parameters for writing statistics
+type stats
+  logical :: calc
+  integer :: nstart, nend !  Time step when to start and stop averaging
+end type stats
+
+!  Reynolds stresses
+type rs
+  logical :: calc
+  double precision, allocatable, dimension(:,:,:) :: up2, vp2, wp2, & 
+                                                     upwp, vpwp, upvp
+end type rs
+
+!  Sums performed over time
+type taver
+  logical :: calc,started
+  integer :: nstart, nend
+  double precision, allocatable, dimension(:,:,:) :: u, v, w, &
+                                                     u2, v2, w2, &
+      											     uw, vw, uv, dudz
+end type taver	
+  
+!  Instantaneous Variables Storage (Parameters for storing velocity 
+!  componentsvalues each time step)
+type ui_pnt
+  logical :: calc,started,global
+  integer :: nstart, nend, nloc, nskip
+  integer :: ijk(3,10) !  Can specify up to 10 points to record 
+end type
+
+!  Instantaneous velocity global declarations
+type ui_gbl
+  logical :: calc,started,global
+  integer :: nstart, nend, nskip
+end type   
+  
+!  Planar stats/data
+type plane
+  logical :: avg
+  integer :: na, nstart, nend
+  integer, dimension(10) :: istart
+  double precision :: fa
+  double precision, dimension (10) :: la, ldiff
+  double precision, allocatable, dimension(:,:,:) :: ua, va, wa
+end type	
+  
+logical :: aver_calc
+ 
+type(stats)          :: stats_t
+type(rs)             :: rs_t
+type(taver)          :: taver_t
+type(ui_pnt), target :: ui_pnt_t
+type(ui_gbl)         :: ui_gbl_t
+type(plane)		     :: yplane_t, zplane_t
+  
+contains
+!***************************************************************
+double precision function interp_to_uv_grid(var,i,j,k)
+!***************************************************************
+!  This function computes any values the read in value u1(k) and
+!  u2(k+1) to the w grid location k
+use param,only : nz
+use sim_param, only : w, dudz
+
+character(*), intent(IN) :: var
+integer,intent(IN) :: i,j,k
+
+if(trim(adjustl(var)) == 'w') then
+  if(k==nz) then
+    interp_to_uv_grid = 3./2.*w(i,j,k) - 0.5*w(i,j,k-1)
+  else
+    interp_to_uv_grid = 0.5*(w(i,j,k)+w(i,j,k+1))
+  endif
+elseif(trim(adjustl(var)) == 'dudz') then 
+  if(k==nz) then
+    interp_to_uv_grid = 3./2.*dudz(i,j,k) - 0.5*dudz(i,j,k-1)
+  else
+    interp_to_uv_grid = 0.5*(dudz(i,j,k)+dudz(i,j,k+1))
+  endif
+else
+  write(*,*) 'Error: variable specification not specified properly!'
+  stop
+endif
+return
+end function
+
+end module stat_defs
+  
+module Sij_defs
+  use param, only : ld, nx, ny, nz, USE_MPI, coord, lbc_mom
+  double precision, dimension (ld, ny, nz) :: S11, S12, S22, S33, S13, S23
+end module Sij_defs
+
