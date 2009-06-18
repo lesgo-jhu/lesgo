bc96b6d851909b5eca92a054ad4af054f88b61c4
jgraham <jgraham@4c8967c9-1b3f-4110-b94f-c20ed91a0040>
2009-06-18 01:31:03 +0000

    Updated grid dimensions for cube_mpi run; generated the phi.out and brindex.out files with trees_pre_ls

--- param.f90
+++ param.f90
@@ -45,7 +45,7 @@ module param
   logical, parameter :: VERBOSE = .false.  !--prints small stuff to screen
   !--use DEBUG to write lots of data/files
 
-  integer,parameter:: nx=64,ny=64,nz=(57 + (nproc-1)-1)/nproc + 1
+  integer,parameter:: nx=64,ny=64,nz=(56 + (nproc-1)-1)/nproc + 1
   integer, parameter :: nz_tot = (nz - 1) * nproc + 1
   integer,parameter:: nx2=3*nx/2,ny2=3*ny/2
   integer,parameter:: lh=nx/2+1,ld=2*lh,lh_big=nx2/2+1,ld_big=2*lh_big
@@ -58,9 +58,10 @@ module param
   real(rprec),parameter::pi=3.1415926535897932384626433_rprec
     !real(rprec),parameter::z_i=1._rprec, L_z=(1._rprec * z_i)/nproc
   real(rprec),parameter::z_i=1._rprec
-  real(rprec),parameter::L_x=4.*z_i, L_y=4.*z_i, L_z=3.587301587301587302*z_i
+  real(rprec),parameter::L_x=4.*z_i, L_y=4.*z_i, L_z=3.5873015873*z_i
   !--L_z is not nondimensionalized by z_i yet
   ! set the aspect ratio of the box, already nondimensional
+!  real(rprec),parameter::dz=L_z/z_i/(nz_tot-1./2.)
   real(rprec),parameter::dz=L_z/z_i/(nz_tot-1./2.)
   real(rprec),parameter::dx=L_x/(nx-1),dy=L_y/(ny-1)
 
