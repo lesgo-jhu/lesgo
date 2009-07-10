8fc233b4f0f3370cb84de00362367da51a192e52
jgraham <jgraham@4c8967c9-1b3f-4110-b94f-c20ed91a0040>
2009-07-10 18:59:46 +0000

    Updated scripts to have a debugging flag

--- cyl_skew-run.sh
+++ cyl_skew-run.sh
@@ -2,14 +2,14 @@
 NPROC=$1
 EXEC=cylinder_skew
 TAIL_NUM=100
-USE_GDB=YES
+USE_GDB=no
 if [ -n "$NPROC" ]; then
   /bin/true
 else
   echo Error: please specify number of processors.
   exit 1
 fi
-if [ $USE_GDB == "YES" ]; then
+if [ $USE_GDB == "yes" ]; then
   mpiexec -gdb -np $NPROC ./$EXEC 
 else
   mpiexec -np $NPROC ./$EXEC >& $EXEC.out < /dev/null &
