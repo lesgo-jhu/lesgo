90f69f4ae7b8e2d0f69d9781f315fb370ede3270
jgraham <jgraham@4c8967c9-1b3f-4110-b94f-c20ed91a0040>
2009-08-26 20:42:40 +0000

    Added argument check

--- utils/cyl_skew_CD_rename.sh
+++ utils/cyl_skew_CD_rename.sh
@@ -1,9 +1,16 @@
-#!/usr/bin/bash
+#!/bin/bash
 FBASE="cylinder_skew_CD.dat"
 FCASE="$1";#"ngen2.200k"
 NPROC=$2;#128;
 NGEN=$3;#2;
-echo $FBASE
+
+#  Check number of input arguments
+if [ $# -ne 3 ]
+then
+  echo "Usage: `basename $0` casename nproc ngen"
+  exit 1
+fi
+
 for (( np=0; np<=$NPROC-1; np++ ))
 do
 	for (( ng=1; ng <= $NGEN; ng++ ))
