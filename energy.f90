f3668d87b21395dfb8901393274761f7627cc203
jgraham <jgraham@4c8967c9-1b3f-4110-b94f-c20ed91a0040>
2010-02-28 21:20:14 +0000

    Returned to basic w interpolation

--- energy.f90
+++ energy.f90
@@ -3,7 +3,6 @@ use types,only:rprec
 use param
 use sim_param,only:u,v,w
 use messages
-use functions, only : interp_to_uv_grid
 $if ($XLF)
   use ieee_arithmetic  !--for NAN checking
 $endif
@@ -41,10 +40,10 @@ denom=2._rprec*(nx*ny*(nz-1))
 z_loop: do jz=1,nz-1
     do jy=1,ny
         do jx=1,nx
-
-!            temp_w=.5_rprec*(w(jx,jy,jz)+w(jx,jy,jz+1))
-!            ke=ke+(u(jx,jy,jz)**2+v(jx,jy,jz)**2+temp_w**2)/denom
-            ke=ke+(u(jx,jy,jz)**2+v(jx,jy,jz)**2+interp_to_uv_grid('w',jx,jy,jz)**2)/denom
+            !  Should perform trilinear interpolation on the cell
+            temp_w=.5_rprec*(w(jx,jy,jz)+w(jx,jy,jz+1))
+            ke=ke+(u(jx,jy,jz)**2+v(jx,jy,jz)**2+temp_w**2)/denom
+            !ke=ke+(u(jx,jy,jz)**2+v(jx,jy,jz)**2+interp_to_uv_grid('w',jx,jy,jz)**2)/denom
             
             $if ($DEBUG)
             if (DEBUG) then
