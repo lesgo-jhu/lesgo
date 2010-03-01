7f65c8a908476215f45858c99efbf78e9ec39f98
cverhul1 <cverhul1@4c8967c9-1b3f-4110-b94f-c20ed91a0040>
2010-03-01 22:21:25 +0000

    Organized parameters based on function: MPI, Domain, Model, Time, BC/IC, Output
    Maintained order for u_star, z_i, dt... (variables that are not defined independently)
    
    This was done to facilitate (future) integration of parameters currently found in subroutine stats_init()

--- param.f90
+++ param.f90
@@ -9,8 +9,10 @@ module param
 
   private rprec  !--this is dumb.
   public
-
-  !--mpi stuff
+  
+!---------------------------------------------------
+! MPI PARAMETERS
+!---------------------------------------------------
   $if ($MPI)
   $define $MPI_LOGICAL .true.
   $define $NPROC 4
@@ -41,7 +43,10 @@ module param
   integer :: coord = -1  !--same here
   integer :: rank_of_coord(0:nproc-1), coord_of_rank(0:nproc-1)
   !--end mpi stuff
-
+  
+!---------------------------------------------------
+! COMPUTATIONAL DOMAIN PARAMETERS
+!---------------------------------------------------  
   integer,parameter:: nx=64,ny=64,nz=(96+4-1)/nproc + 1
   integer, parameter :: nz_tot = (nz - 1) * nproc + 1
   integer,parameter:: nx2=3*nx/2,ny2=3*ny/2
@@ -49,8 +54,6 @@ module param
 
   integer, parameter :: iBOGUS = -1234567890  !--NOT a new Apple product
   real (rprec), parameter :: BOGUS = -1234567890._rprec
-  
-  integer, parameter :: nsteps = 1000
 
   real(rprec),parameter::pi=3.1415926535897932384626433_rprec
     !real(rprec),parameter::z_i=1._rprec, L_z=(1._rprec * z_i)/nproc
@@ -64,44 +67,10 @@ module param
   real(rprec),parameter::dz=nproc*L_z/z_i/(nz_tot-1./2.)
   real(rprec),parameter::dx=L_x/(nx-1),dy=L_y/(ny-1)
 
-  ! u_star=0.45 if coriolis_forcing=.FALSE. and =ug if coriolis_forcing=.TRUE.
-  real(rprec),parameter::u_star=0.45_rprec,Pr=.4_rprec
   
-  real (rprec), parameter :: dt = 2.e-4
-  real (rprec), parameter :: dt_dim = dt*z_i/u_star
-  
-!  real(rprec),parameter::dt_dim=0.1 !dimensional time step in seconds
-!  real(rprec),parameter::dt=dt_dim*u_star/z_i
-                                  !--dt=2.e-4 usually works for 64^3
-  
-  !--Coriolis stuff
-  ! coriol=non-dim coriolis parameter,
-  ! ug=horiz geostrophic vel, vg=transverse geostrophic vel
-  logical,parameter::coriolis_forcing=.false.
-  real(rprec),parameter::coriol=9.125E-05*z_i/u_star,      &
-       ug=u_star/u_star,vg=0._rprec/u_star
-
-  real(rprec),parameter::vonk=0.4_rprec 
-  integer,parameter::c_count=10000,p_count=10000
- !integer, parameter :: cs_count = 1  !--tsteps between dynamic Cs updates
-  integer, parameter :: cs_count = 5  !--tsteps between dynamic Cs updates
-  logical,parameter:: output=.true.
-  logical, parameter :: use_avgslice = .false.
-  !  Set minimum time step to write averaged slices
-  integer, parameter :: avgslice_start = 0
-  
-  
-  !--initu = true to read from a file; false to create with random noise
-  logical, parameter :: initu = .true.
-  !--initlag = true to initialize cs, FLM & FMM; false to read from vel.out
-  logical, parameter :: inilag = .false.
-
-  ! nu_molec is dimensional m^2/s
-  real(rprec),parameter::nu_molec=1.14e-5_rprec
-
-  logical,parameter::use_bldg=.false.
-  logical,parameter::molec=.false.,sgs=.true.,dns_bc=.false.
-
+!---------------------------------------------------
+! MODEL PARAMETERS
+!---------------------------------------------------   
   !Model type: 1->Smagorinsky; 2->Dynamic; 3->Scale dependent
   !            4->Lagrangian scale-sim   5-> Lagragian scale-dep
   !Models type: 1->static prandtl, 2->Dynamic
@@ -111,19 +80,67 @@ module param
   real(kind=rprec),parameter::Co=0.16_rprec
 
   !Test filter type: 1->cut off 2->Gaussian 3->Top-hat
-  integer,parameter::ifilter=2
+  integer,parameter::ifilter=2	 
+
+  ! u_star=0.45 if coriolis_forcing=.FALSE. and =ug if coriolis_forcing=.TRUE.
+  real(rprec),parameter::u_star=0.45_rprec,Pr=.4_rprec
+
+  !--Coriolis stuff
+  ! coriol=non-dim coriolis parameter,
+  ! ug=horiz geostrophic vel, vg=transverse geostrophic vel
+  logical,parameter::coriolis_forcing=.false.
+  real(rprec),parameter::coriol=9.125E-05*z_i/u_star,      &
+       ug=u_star/u_star,vg=0._rprec/u_star
+	   
+  real(rprec),parameter::vonk=0.4_rprec 
+  integer,parameter::c_count=10000,p_count=10000
+ !integer, parameter :: cs_count = 1  !--tsteps between dynamic Cs updates
+  integer, parameter :: cs_count = 5  !--tsteps between dynamic Cs updates	   
+  
+  ! nu_molec is dimensional m^2/s
+  real(rprec),parameter::nu_molec=1.14e-5_rprec  
+	   
+  logical,parameter::use_bldg=.false.
+  logical,parameter::molec=.false.,sgs=.true.,dns_bc=.false.  
+  
+!---------------------------------------------------
+! TIMESTEP PARAMETERS
+!---------------------------------------------------   
+  integer, parameter :: nsteps = 1000
+ 
+  real (rprec), parameter :: dt = 2.e-4
+  real (rprec), parameter :: dt_dim = dt*z_i/u_star
+  
+!  real(rprec),parameter::dt_dim=0.1 !dimensional time step in seconds
+!  real(rprec),parameter::dt=dt_dim*u_star/z_i
+                                  !--dt=2.e-4 usually works for 64^3
+  
+  integer :: jt  ! global time-step counter
+  integer :: jt_total  !--used for cumulative time (see io module)
+
+  ! time advance parameters (AB2)
+  real (rprec), parameter :: tadv1 = 1.5_rprec, tadv2 = 1._rprec - tadv1								  
+								  
+								  
+!---------------------------------------------------
+! BOUNDARY/INITIAL CONDITION PARAMETERS
+!---------------------------------------------------  
+  !--initu = true to read from a file; false to create with random noise
+  logical, parameter :: initu = .true.
+  !--initlag = true to initialize cs, FLM & FMM; false to read from vel.out
+  logical, parameter :: inilag = .false.								  
 
   ! ubc: upper boundary condition: ubc=0 stress free lid, ubc=1 sponge
   integer,parameter::ubc=0
 
   character (*), parameter :: lbc_mom = 'wall'
   !--'wall', 'stress free'
-
+  
   !--prescribed inflow: constant or read from file
   !  read from file is not working properly
   logical,parameter::inflow=.false.
-  logical, parameter :: use_fringe_forcing = .false.
-
+  logical, parameter :: use_fringe_forcing = .false.  
+  
   real (rprec), parameter :: buff_end = 1._rprec
   !--position of right end of buffer region,
   !  as a fraction of L_x
@@ -145,14 +162,19 @@ module param
 
   logical, parameter :: use_mean_p_force = .true.
   real (rprec), parameter :: mean_p_force = 1._rprec * z_i/(nproc*L_z)
-  !--usually just z_i/L_z
-
-  integer :: jt  ! global time-step counter
-  integer :: jt_total  !--used for cumulative time (see io module)
-
-  ! time advance parameters (AB2)
-  real (rprec), parameter :: tadv1 = 1.5_rprec, tadv2 = 1._rprec - tadv1
-
+  !--usually just z_i/L_z  
+  
+!---------------------------------------------------
+! DATA OUTPUT PARAMETERS
+!---------------------------------------------------  
+  logical,parameter:: output=.true.
+  logical, parameter :: use_avgslice = .false.
+  !  Set minimum time step to write averaged slices
+  integer, parameter :: avgslice_start = 0
+ 
+ ! COMING SOON: parameters from subroutine stats_init()
+  
+  
   !------xxxxxxxxx--SCALARS_PARAMETERS--xxxxxxxxx---------------
   ! S_FLAG=1 for Theta and q, =0 for no scalars
   !logical,parameter::S_FLAG=.TRUE.,coupling_flag=.FALSE.,mo_flag=.TRUE.
