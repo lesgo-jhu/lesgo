b9ea148949fd673393cf479076329b125b607fb3
cverhul1 <cverhul1@4c8967c9-1b3f-4110-b94f-c20ed91a0040>
2010-03-03 17:53:39 +0000

    Basic module structure

--- turbines.f90
+++ turbines.f90
@@ -1,1 +1,29 @@
-! coming soon
+module turbines
+use types,only:rprec
+use param
+!use test_filtermodule --subroutines are not actually part of this module (yet)
+
+implicit none
+
+save
+private
+
+public :: turbines_forcing
+
+!Filter type: 1->cut off 2->Gaussian 3->Top-hat
+integer,parameter::turbines_filter_type=2
+real (rprec) :: turbines_filter_size=1.5
+
+
+!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+contains
+!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
+subroutine turbines_forcing()
+!locate applicable nodes (another subroutine, probably)
+!calculate forcing
+!filter forcing
+!apply forcing
+end subroutine
+
+end module turbines
