472fa42dce329380fb2ba0eeda82b5d88c2b8686
jgraham <jgraham@4c8967c9-1b3f-4110-b94f-c20ed91a0040>
2010-03-09 00:37:28 +0000

    Set default write action to true

--- rns_ls.f90
+++ rns_ls.f90
@@ -37,7 +37,7 @@ call mesg ( sub_name, 'reading rns_planes' )
 !  Set the number of trees to use
 rns_t % ntrees = 1
 !  Set plane average write
-rns_t % plane_u_calc = .false.
+rns_t % plane_u_calc = .true.
 
 do nt=1, rns_t % ntrees
   
@@ -72,7 +72,7 @@ use sim_param, only : u, v, w
 use functions, only : plane_avg_3D
 use param, only : jt_total, dt_dim, Ny, Nz_tot, L_y, L_z, nproc
 use io, only : w_uv, w_uv_tag, dudz_uv, dudz_uv_tag
-use io, only : write_tecplot_header_ND, write_real_data, interp_to_uv_grid
+use io, only : write_tecplot_header_xyline, write_real_data, interp_to_uv_grid
 implicit none
 
 character(*), parameter :: fbase= path // 'output/uvw_rns_planes.dat'
@@ -105,10 +105,10 @@ do np = 1, rns_t%nplanes
   if(coord == 0) then
   
     inquire (file=fname, exist=exst)
-	!if(.not. exst) call write_tecplot_header_xyline(fname, 'rewind', &
-	    !'"t (s)", "u", "v", "w"')
-	if (.not. exst) call write_tecplot_header_ND(fname, 'rewind', 4, (/ Nx /), '"t (s)", "u", "v", "w"', &
-		  np, 2)
+	if(.not. exst) call write_tecplot_header_xyline(fname, 'rewind', &
+	    '"t (s)", "u", "v", "w"')
+	!if (.not. exst) call write_tecplot_header_ND(fname, 'rewind', 4, (/ Nx /), '"t (s)", "u", "v", "w"', &
+	!	  np, 2)
 
 	call write_real_data(fname, 'append', 4, (/ jt_total*dt_dim, &
 	  rns_planes_t(np)%u, rns_planes_t(np)%v, rns_planes_t(np)%w /))
