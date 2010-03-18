e8c1ae4369b9d8b5cb56c94ed247e697ba82a6c8
cverhul1 <cverhul1@4c8967c9-1b3f-4110-b94f-c20ed91a0040>
2010-03-18 23:29:59 +0000

    Added my own FFT and implemented filtering in x-y planes.  These changes need to be tested in a simpler program.

--- turbines.f90
+++ turbines.f90
@@ -35,10 +35,10 @@ allocate(turbine_t%theta2(turbine_t%nloc))
 	turbine_t%yloc(2) = L_y/2.
 	turbine_t%theta1(2) = 0.
 	turbine_t%theta2(2) = 0.
-turbine_t%ifilter_h = 2			!Filter type: 1->cut off 2->Gaussian 3->Top-hat
-turbine_t%ifilter_v = 4			!Filter type: 4->Truncated Gaussian		
-turbine_t%alpha_h = 1.5			!filter size is alpha*(grid spacing)
-turbine_t%alpha_v = 1.5  			!h=horizontal (xy), v=vertical (xz,yz)
+turbine_t%ifilter_xy = 2			!Filter type: 1->cut off 2->Gaussian 3->Top-hat
+turbine_t%ifilter_z = 4				!Filter type: 4->Truncated Gaussian		
+turbine_t%alpha_xy = 1.5			!filter size is alpha*(grid spacing)
+turbine_t%alpha_z = 1.5  			!h=horizontal (xy), v=vertical (xz,yz)
 
 real(rprec), parameter :: Ct = 1.33		!thrust coefficient
 !#########################################################################################
@@ -49,6 +49,10 @@ real, dimension (turbine_t%nloc) :: n_hat			!(nx,ny,nz) of unit normal for each 
 real, dimension(turbine_t%nloc) :: u_d_T			!running time-average of mean disk velocity
 integer :: flag										!indicates fist time step (where u_d_T = u_d)
 real :: eps											!epsilon used for disk velocity time-averaging
+
+real, dimension(nx/2+1,ny) :: G_turbines_xy			!for filtering forces (transfer function)
+integer*8::forw_xy,back_xy							!FFT plans
+real, dimension(nx/2+1,ny)::kxT,kyT,k2T				!wavenumbers
 
 !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 contains
@@ -61,8 +65,11 @@ use param, only: dt, L_z, u_star
 !locate applicable nodes
 	call turbines_nodes()
 
+!initialize FFT (create plans and initialize wavenumbers)
+	call turbines_fft()	
+	
 !initialize filter	
-	call turbines_filter_init(turbine_t%alpha_xy,G_turbines_xy,turbine_t%ifilter_xy)
+	call turbines_filter_init()
 
 !initialize variables
 	u_d_T = 0.
@@ -124,10 +131,11 @@ integer :: i,j,k,m,l,i2,j2,k2,count_i
 		count_i = count_i + num_nodes(m)
 	enddo
 		
-!filter forcing 
+!filter forcing (currently only in the x-y planes)
 	do k=1,nz
-		call turbines_filter(f_x(:,:,k),G_turbines_xy)  ! note: this filters in-place, so input is ruined	
-		call turbines_filter(f_y(:,:,k),G_turbines_xy) 
+		call turbines_filter_xy(f_x(:,:,k))		!filters in place so input is ruined
+		call turbines_filter_xy(f_y(:,:,k)) 
+		call turbines_filter_xy(f_z(:,:,k)) 
 	enddo
 
 !apply forcing to RHS
@@ -191,55 +199,98 @@ end subroutine turbines_nodes
 
 !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 
-subroutine turbines_filter_init(alpha,G_test)
-! spectral cutoff filter at width alpha*delta
+subroutine turbines_filter_init()
+! G_turbines should take care of normalization and Nyquist
 ! note the normalization for FFT's is already in G! (see 1/(nx*ny))
-use types,only:rprec
-use fft
-implicit none
+! currently: only prepares G_turbines_xy (not "_z)
+use param, only: nx,ny,dx,dy,pi
 
-real(kind=rprec):: alpha, delta, kc2
-real(kind=rprec),dimension(lh,ny) :: G_test
+real(kind=rprec):: delta, kc2
 
-G_test=1._rprec/(nx*ny)  ! normalization for the forward FFT
-delta = alpha*sqrt(dx*dy)  ! "2d-delta", not full 3d one
+	G_turbines_xy=1._rprec/(nx*ny)  		! normalization for the forward FFT
+	delta = turbine_t%alpha_xy*sqrt(dx*dy)  ! "2d-delta", not full 3d one
 
-if(ifilter==1) then ! spectral cutoff filter
-! alpha is ratio of test filter to grid filter widths
-   kc2 = (pi/(delta))**2
-   where (real(k2, rprec) >= kc2) G_test = 0._rprec
-else if(ifilter==2) then ! Gaussian filter
-   G_test=exp(-(2._rprec*delta)**2*k2/(4._rprec*6._rprec))*G_test       
-else if(ifilter==3) then ! Top-hat (Box) filter
-   G_test= (sin(kx*delta/2._rprec)*sin(ky*delta/2._rprec)+1E-8)/&
-        (kx*delta/2._rprec*ky*delta/2._rprec+1E-8)*G_test
-endif
-! since our k2 has zero at Nyquist, we have to do this by hand
-   G_test(lh,:) = 0._rprec
-   G_test(:,ny/2+1) = 0._rprec
+! 1. Spectral cutoff filter
+	if(turbine_t%ifilter_xy==1) then 
+		kc2 = (pi/(delta))**2
+		where (real(k2T, rprec) >= kc2) G_turbines_xy = 0._rprec
+! 2. Gaussian filter
+	else if(turbine_t%ifilter_xy==2) then 
+		G_turbines_xy=exp(-(2._rprec*delta)**2*k2T/(4._rprec*6._rprec))*G_turbines_xy       
+! 3. Top-hat (Box) filter
+	else if(turbine_t%ifilter_xy==3) then 
+		G_turbines_xy= (sin(kxT*delta/2._rprec)*sin(kyT*delta/2._rprec)+1E-8)/&
+        (kxT*delta/2._rprec*kyT*delta/2._rprec+1E-8)*G_turbines_xy
+	endif
+
+! since our k2T has zero at Nyquist, we have to do this by hand
+	G_turbines_xy(nx/2+1,:) = 0._rprec
+	G_turbines_xy(:,ny/2+1) = 0._rprec
 
 end subroutine turbines_filter_init
 
 !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 
-subroutine turbines_filter(f,G_test)
-! note: this filters in-place, so input is ruined
-use types,only:rprec
-use param,only:lh,ny
-use fft
-implicit none
-! note we're treating as complex here
-complex(kind=rprec), dimension(lh,ny),intent(inout)::f
-real(kind=rprec), dimension(lh,ny),intent(in) :: G_test
+subroutine turbines_filter_xy(real_a)
+use param,only:nx,ny
 
-real (rprec) :: ignore_me
+real, dimension(nx,ny), intent(inout) :: real_a
+complex, dimension(nx/2+1,ny) :: complex_a
 
-call rfftwnd_f77_one_real_to_complex(forw,f,ignore_me)
-! the normalization is "in" G_test
-f = G_test*f  ! Nyquist taken care of with G_test
-call rfftwnd_f77_one_complex_to_real(back,f,ignore_me)
+!perform FFT (physical to wavenumber space)
+	call rfftwnd_f77_one_real_to_complex(forw_xy, real_a, complex_a)
+!perform filter convolution
+	complex_a = complex_a * G_turbines_xy
+!perform FFT (wavenumber to physical space)
+	call rfftwnd_f77_one_complex_to_real(back_xy, complex_a, real_a)
+	
+!normalization is part of G_turbines (check that it is done correctly)
 
-end subroutine turbines_filter
+end subroutine turbines_filter_xy
+
+!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
+subroutine turbines_fft()
+!arrays should be in column-major order
+use param,only:nx,ny,L_x,L_y,pi
+
+integer, parameter :: FFTW_REAL_TO_COMPLEX=-1,FFTW_COMPLEX_TO_REAL=1
+integer, parameter :: FFTW_ESTIMATE=0,FFTW_MEASURE=1
+integer, parameter :: FFTW_OUT_OF_PLACE=0
+integer, parameter :: FFTW_IN_PLACE=8,FFTW_USE_WISDOM=16
+integer, parameter :: FFTW_THREADSAFE=128
+
+integer :: jx,jy
+
+!create my own plans for FFT (that don't filter in place)
+	call rfftw2d_f77_create_plan(forw_xy,nx,ny,FFTW_REAL_TO_COMPLEX,&
+     FFTW_MEASURE+FFTW_THREADSAFE)
+	call rfftw2d_f77_create_plan(back_xy,nx,ny,FFTW_COMPLEX_TO_REAL,&
+     FFTW_MEASURE+FFTW_THREADSAFE)
+
+!also initialize wavenumber arrays - to be used during filtering?
+	do jx=1,(nx/2+1)-1
+		kxT(jx,:) = real(jx-1,kind=rprec)
+	end do
+
+	do jy=1,ny
+	kyT(:,jy) = real(modulo(jy - 1 + ny/2,ny) - ny/2,kind=rprec)
+	end do
+
+	! Nyquist: makes doing derivatives easier
+    kxT(nx/2+1,:)=0._rprec
+    kyT(nx/2+1,:)=0._rprec
+    kxT(:,ny/2+1)=0._rprec
+    kyT(:,ny/2+1)=0._rprec
+	  
+	! for the aspect ratio change
+    kxT=2._rprec*pi/L_x*kx
+    kyT=2._rprec*pi/L_y*ky
+	  
+	! magnitude squared: will have 0's around the edge
+    k2T = kx*kx + ky*ky
+
+end subroutine turbines_fft
 
 !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 
