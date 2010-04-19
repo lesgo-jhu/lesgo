7f58a80e344d94fa67277ae9288292ab4a91749f
(no author) <(no author)@4c8967c9-1b3f-4110-b94f-c20ed91a0040>
2010-04-19 16:06:24 +0000

    This commit was manufactured by cvs2svn to create tag 'pre_full_sgs_model'.

--- loadleveler/mss.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-msrcp -pe 90 -wpwd script -rpwd script r33--4zone--D-clip0.tgz \
-  mss:/SCHESTER/3d-les-tree/r0.33/4zone/r33--4zone--D-clip0.tgz
-msrcp -pe 90 -wpwd script -rpwd script r33--4zone--G-clip0.tgz \
-  mss:/SCHESTER/3d-les-tree/r0.33/4zone/r33--4zone--G-clip0.tgz
--- stats.f90
+++
@@ -1,6 +0,0 @@
-! This file formerly contained the following subroutines:
-!   tsum_compute()
-!   tavg_compute()
-!   rs_compute()
-!   plane_avg_compute(jt)
-! They have been moved to io.f90           [CV 3/2/10]
