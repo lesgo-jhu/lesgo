b12f7918563e2b344863f296ba1eb7d9df3102c6
jgraham <jgraham@4c8967c9-1b3f-4110-b94f-c20ed91a0040>
2010-06-03 16:54:45 +0000

    Corrected spacing

--- stat_defs.f90
+++ stat_defs.f90
@@ -7,7 +7,7 @@ save
 public
 
 type rs
-        real(rprec) :: up2, vp2, wp2, upwp, vpwp, upvp
+  real(rprec) :: up2, vp2, wp2, upwp, vpwp, upvp
 end type rs
 
 real(rprec) :: tavg_total_time
@@ -57,26 +57,26 @@ $if ($TURBINES)
 	type turbine 
 		real(rprec) :: xloc, yloc, height, dia, thk		  
 		real(rprec) :: theta1                       !angle CCW(from above) from -x direction [degrees]
-        real(rprec) :: theta2	                    !angle above the horizontal, from -x dir [degrees]
+    real(rprec) :: theta2	                    !angle above the horizontal, from -x dir [degrees]
 		real(rprec), dimension(3) :: nhat           !(nx,ny,nz) of unit normal for each turbine
 		integer :: num_nodes                        !number of nodes associated with each turbine
 		integer, dimension(1500,3) :: nodes         !(i,j,k) of each included node
-        integer, dimension(6) :: nodes_max          !search area for nearby nodes
+    integer, dimension(6) :: nodes_max          !search area for nearby nodes
 		real(rprec) :: u_d, u_d_T                   !running time-average of mean disk velocity
-        real(rprec) :: f_n                          !normal force on turbine disk
-        integer :: u_d_flag
+    real(rprec) :: f_n                          !normal force on turbine disk
+    integer :: u_d_flag
 		real(rprec), dimension(1500) :: ind                !indicator function - weighting of each node
 	end type turbine
 
 	type wind_farm
 		integer ::ifilter                           !Filter type: 2->Gaussian
 		real(rprec) :: alpha                        !filter size is alpha*(grid spacing)
-        integer :: trunc                            !truncated - # grid points to include in each dir.
-        real(rprec) :: filter_cutoff                !ind only includes values above this cutoff
+    integer :: trunc                            !truncated - # grid points to include in each dir.
+    real(rprec) :: filter_cutoff                !ind only includes values above this cutoff
 		type(turbine), pointer, dimension(:) :: turbine_t
 	end type wind_farm	
     
-    type(wind_farm)	        :: wind_farm_t	
+  type(wind_farm)	        :: wind_farm_t	
 $endif
 
 type(tavg), pointer, dimension(:,:,:) :: tavg_t
