3c278fd69a854e43994c3853da83fbbe9caed5b8
jgraham <jgraham@4c8967c9-1b3f-4110-b94f-c20ed91a0040>
2010-06-11 16:16:59 +0000

    Updated definitions of primary and secondary RNS structures

--- rns_base_ls.f90
+++ rns_base_ls.f90
@@ -48,18 +48,31 @@ type indx_array
   integer :: npoint
   integer, pointer, dimension(:,:) :: iarray
 end type
+
+type child_elem
+  integer :: nelem
+  integer, pointer, dimension(:) :: iarray
+end type child_elem
 ! ---- Secondary structures ----
 
 ! ---- Primary structures ----
-type primary_struct
+type primary_struct_type_1
   type(ref_plane)   :: ref_plane_t
   type(force)       :: force_t
   type(indx_array)  :: indx_array_t
-end type primary_struct
+end type primary_struct_type_1
 
-type(primary_struct), pointer, dimension(:) :: reslv_elem_t
-type(primary_struct), pointer, dimension(:) :: beta_elem_t
-type(primary_struct), pointer, dimension(:) :: rbeta_elem_t
+type primary_struct_type_2
+  type(ref_region)   :: ref_region_t
+  type(force)       :: force_t
+  type(indx_array)  :: indx_array_t
+  type(child_elem)  :: r_child_t
+  type(child_elem)  :: beta_child_t
+end type primary_struct_type_2
+
+type(primary_struct_type_1), pointer, dimension(:) :: r_elem_t
+type(primary_struct_type_1), pointer, dimension(:) :: beta_elem_t
+type(primary_struct_type_2), pointer, dimension(:) :: b_elem_t
 ! ---- Primary structures ----
 
 
