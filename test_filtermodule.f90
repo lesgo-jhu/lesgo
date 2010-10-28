1e5be8ef7a43287620eaf7e855aa334c5c193687
jgraham <jgraham@4c8967c9-1b3f-4110-b94f-c20ed91a0040>
2010-10-28 21:07:19 +0000

    Updated test_filter to use real arrays

--- test_filtermodule.f90
+++ test_filtermodule.f90
@@ -11,29 +11,33 @@ end module test_filtermodule
 
 !--THIS IS NOT IN THE MODULE
 !**********************************************************************
-subroutine test_filter(f,G_test)
+subroutine test_filter(f, G_test)
 !**********************************************************************
 ! note: this filters in-place, so input is ruined
 use types,only:rprec
 use param,only:ld,lh,ny
 use fft
 implicit none
-! note we're treating as complex here
-!complex(kind=rprec), dimension(lh,ny),intent(inout)::f
+
+!complex(kind=rprec), dimension(lh,ny),intent(inout)::f ! note we're treating as complex here
 real(kind=rprec), dimension(ld,ny), intent(inout) :: f
 real(kind=rprec), dimension(lh,ny),intent(in) :: G_test
 
-!f_c=cmplx(f,0.,kind=rprec)
+!  Perform in-place FFT
 call rfftwnd_f77_one_real_to_complex(forw,f,null())
 
-! the normalization is "in" G_test
-!f = G_test*f  ! Nyquist taken care of with G_test
+!  Perform f = G_test*f, emulating f as complex
+! Nyquist frequency and normalization is taken care of with G_test
 call emul_complex_mult_inplace_real_complex_real_2D( f, G_test, ld, lh, ny )
+
 call rfftwnd_f77_one_complex_to_real(back,f,null())
 
+return
 end subroutine test_filter
 
+!**********************************************************************
 subroutine test_filter_init(alpha,G_test)
+!**********************************************************************
 ! spectral cutoff filter at width alpha*delta
 ! note the normalization for FFT's is already in G! (see 1/(nx*ny))
 use types,only:rprec
@@ -61,4 +65,6 @@ endif
 ! since our k2 has zero at Nyquist, we have to do this by hand
    G_test(lh,:) = 0._rprec
    G_test(:,ny/2+1) = 0._rprec
+
+return   
 end subroutine test_filter_init
