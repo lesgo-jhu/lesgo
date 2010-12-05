537d95100ca1fd6ec8705f044d51d5aeb947862b
jgraham <jgraham@4c8967c9-1b3f-4110-b94f-c20ed91a0040>
2010-12-05 16:21:08 +0000

    Pulled from cuda_fft_branch; fixes issue with PGI compiler using kludge

--- cfl_util.f90
+++ cfl_util.f90
@@ -11,16 +11,54 @@ use param, only : up, down, ierr, MPI_RPREC, status, comm, coord
 $endif
 
 implicit none
-
 real(rprec), intent(OUT) :: cfl
+$if($PGI)
+integer :: i, j, k, icount
+real(rprec), allocatable, dimension(:) :: u_inter
+$endif
 
 $if($MPI)
 real(rprec) :: cfl_buf
 $endif
 
-cfl = maxval( (/ abs(u(1:nx,1:ny,1:nz-1)) / dx, &
-                 abs(v(1:nx,1:ny,1:nz-1)) / dy, &
-                 abs(w(1:nx,1:ny,1:nz-1)) / dz /) )
+$if($PGI)
+allocate(u_inter(nx*ny*(nz-1)*3))
+icount=0
+do k=1,nz-1
+  do j=1,ny
+    do i=1,nx
+      icount = icount + 1
+      u_inter(icount) = abs(u(i,j,k)) / dx
+    enddo
+  enddo
+enddo
+do k=1,nz-1
+  do j=1,ny
+    do i=1,nx
+      icount = icount + 1
+      u_inter(icount) = abs(v(i,j,k)) / dy
+    enddo
+  enddo
+enddo
+do k=1,nz-1
+  do j=1,ny
+    do i=1,nx
+      icount = icount + 1
+      u_inter(icount) = abs(w(i,j,k)) / dz
+    enddo
+  enddo
+enddo
+
+$endif
+
+$if($PGI)
+cfl = maxval( u_inter )
+deallocate( u_inter )
+$else
+cfl = maxval( (/ dabs(u(1:nx,1:ny,1:nz-1)) / dx, &
+                 dabs(v(1:nx,1:ny,1:nz-1)) / dy, &
+                 dabs(w(1:nx,1:ny,1:nz-1)) / dz /) )
+$endif
 
 cfl = dt * cfl
 
@@ -49,13 +87,52 @@ implicit none
 
 real(rprec), intent(OUT) :: dt
 
+$if($PGI)
+integer :: i, j, k, icount
+real(rprec), allocatable, dimension(:) :: u_inter
+$endif
+
 $if($MPI)
 real(rprec) :: dt_buf
 $endif
 
+$if($PGI)
+allocate(u_inter(nx*ny*(nz-1)*3))
+icount=0
+do k=1,nz-1
+  do j=1,ny
+    do i=1,nx
+      icount = icount + 1
+      u_inter(icount) = dx / abs(u(i,j,k))
+    enddo
+  enddo
+enddo
+do k=1,nz-1
+  do j=1,ny
+    do i=1,nx
+      icount = icount + 1
+      u_inter(icount) = dy / abs(v(i,j,k))
+    enddo
+  enddo
+enddo
+do k=1,nz-1
+  do j=1,ny
+    do i=1,nx
+      icount = icount + 1
+      u_inter(icount) = dz / abs(w(i,j,k))
+    enddo
+  enddo
+enddo
+$endif
+
+$if($PGI)
+dt = minval( u_inter )
+deallocate(u_inter)
+$else
 dt = minval( (/ dx / abs(u(1:nx,1:ny,1:nz-1)), &
                 dy / abs(v(1:nx,1:ny,1:nz-1)), &
                 dz / abs(w(1:nx,1:ny,1:nz-1)) /) )
+$endif
 
 dt = cfl * dt
 
