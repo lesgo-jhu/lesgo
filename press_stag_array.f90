312c64fe7e1cc29a8918613065ae07583509cb91
jgraham <jgraham@4c8967c9-1b3f-4110-b94f-c20ed91a0040>
2011-06-02 06:08:21 +0000

    Removed all PC_SCHEME_* stuff

--- press_stag_array.f90
+++ press_stag_array.f90
@@ -24,11 +24,7 @@ subroutine press_stag_array(p_hat,dfdx,dfdy)
 !-------------------          
 use types,only:rprec
 use param
-use messages
 use sim_param,only:u,v,w,divtz
-$if($PC_SCHEME_1)
-use sim_param, only : dpdx_f, dpdy_f, dpdz_f
-$endif
 use fft
 use immersedbc,only:fx,fy,fz  ! only for forcing experiment
 
@@ -67,8 +63,6 @@ integer :: jz_min
 complex(kind=rprec),dimension(lh, ny, nz+1)::RHS_col
 real(kind=rprec),dimension(lh, ny, nz+1)::a,b,c
 
-character(*), parameter :: sub_name='press_stag_array'
-
 !---------------------------------------------------------------------
 $if ($VERBOSE)
 write (*, *) 'started press_stag_array'
@@ -88,30 +82,10 @@ do jz=1,nz-1  !--experiment: was nz here (see below experiments)
 ! temp storage for sum of RHS terms.  normalized for fft
 ! sc: recall that the old timestep guys already contain the pressure
 !   term
-
-   $if($PC_SCHEME_0)
-   ! Original PC
+   ! no forces
    rH_x(:, :, jz) = const / tadv1 * (u(:, :, jz) / dt)
    rH_y(:, :, jz) = const / tadv1 * (v(:, :, jz) / dt)
    rH_z(:, :, jz) = const / tadv1 * (w(:, :, jz) / dt)
-
-   $elseif($PC_SCHEME_1)
-   ! Updated PC
-   rH_x(:, :, jz) = const  * (u(:, :, jz) / (tadv1*dt) + dpdx_f(:,:,jz))
-   rH_y(:, :, jz) = const  * (v(:, :, jz) / (tadv1*dt) + dpdy_f(:,:,jz))
-   rH_z(:, :, jz) = const  * (w(:, :, jz) / (tadv1*dt) + dpdz_f(:,:,jz))
-
-   $elseif($PC_SCHEME_2)
-   ! Updated PC-2
-   rH_x(:, :, jz) = const * u(:, :, jz) / dt
-   rH_y(:, :, jz) = const * v(:, :, jz) / dt
-   rH_z(:, :, jz) = const * w(:, :, jz) / dt
- 
-   $else
-
-   call error(sub_name,'Makefile pressure correction scheme flag not set properly')
-
-   $endif
 
    call rfftwnd_f77_one_real_to_complex(forw,rH_x(:,:,jz),ignore_me)
    call rfftwnd_f77_one_real_to_complex(forw,rH_y(:,:,jz),ignore_me)
