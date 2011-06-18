1a2ad7bb845bdacf017ef566f557e719b03c6b44
jgraham <jgraham@4c8967c9-1b3f-4110-b94f-c20ed91a0040>
2011-06-18 04:02:22 +0000

    Updated defaults; also added comments to indicate original, default settings

--- level_set_base.f90
+++ level_set_base.f90
@@ -19,22 +19,24 @@ private :: rp, ld, ny, nz, dx
 !private
 !public :: phi
 
-logical, parameter :: global_CD_calc = .true. ! Compute global CD based on inflow velocity
+logical, parameter :: global_CD_calc = .false. ! Compute global CD based on inflow velocity
 integer, parameter :: Ldir = 2
                       !--lift direction:
                       !  2 when lift direction is y
                       !  3 when lift direction is z
 
 logical, parameter :: vel_BC = .false. !--means we are forcing velocity for
-                                       !  level set BC
-logical, parameter :: use_log_profile = .false.
-logical, parameter :: use_enforce_un = .false.
-logical, parameter :: physBC = .true.
-logical, parameter :: use_smooth_tau = .true.
-logical, parameter :: use_extrap_tau_log = .false.
-logical, parameter :: use_extrap_tau_simple = .true.
+                                       !  level set BC 
+                                       !  (default = .true.)
+logical, parameter :: use_log_profile = .false.       !  (default = .false.)
+logical, parameter :: use_enforce_un = .false.        !  (default = .false.)
+logical, parameter :: physBC = .true.                 !  (default = .true.)        
+logical, parameter :: use_smooth_tau = .true.         !  (default = .true.)
+logical, parameter :: use_extrap_tau_log = .false.    !  (default = .false.)
+logical, parameter :: use_extrap_tau_simple = .true.  !  (default = .true.)
 logical, parameter :: use_modify_dutdn = .false.  !--only works w/interp_tau; not MPI compliant
                                                   !--wont work w/extra_tau_log
+                                                  !  (default = .false.)
 
 ! Enables scale dependent Cs evaluations (not dynamic evaluation)
 ! Used when model=4 in param module
@@ -46,6 +48,7 @@ logical, parameter :: lag_dyn_modify_beta = .true.
 character (*), parameter :: smooth_mode = 'xy'  !--'xy', '3d'
 
 real (rp), parameter :: z0 = 0.0001_rp !--nondimensional roughness length of surface
+!real (rp), parameter :: z0 = 1.6e-3_rp 
 
 logical :: phi_cutoff_is_set = .false.
 logical :: phi_0_is_set = .false.
