78cdba05d2c9bf89bd241c771b3db37755534218
jgraham <jgraham@4c8967c9-1b3f-4110-b94f-c20ed91a0040>
2011-11-30 16:46:04 +0000

    Added level_set block for input.
    Update level_set_base definitions to be read in; also initialized them to defaults. Also renamed 'z0' to 'zo_level_set' to be more explict.

--- level_set_base.f90
+++ level_set_base.f90
@@ -12,37 +12,58 @@ private :: rp, ld, ny, nz, dx
 !private
 !public :: phi
 
-logical, parameter :: global_CD_calc = .true. ! Compute global CD based on inflow velocity
-integer, parameter :: Ldir = 2
-                      !--lift direction:
+!logical, parameter :: global_CD_calc = .true. ! Compute global CD based on inflow velocity
+logical :: global_CD_calc = .false. ! Compute global CD based on inflow velocity
+
+!integer, parameter :: Ldir = 2
+integer :: Ldir = 2   !--lift direction:
                       !  2 when lift direction is y
                       !  3 when lift direction is z
 
 
-logical, parameter :: vel_BC = .false. !--means we are forcing velocity for
-                                       !  level set BC 
-                                       !  (default = .false.)
-logical, parameter :: use_log_profile = .false.       !  (default = .false.)
-logical, parameter :: use_enforce_un = .false.        !  (default = .false.)
-logical, parameter :: physBC = .true.                 !  (default = .true.)        
-logical, parameter :: use_smooth_tau = .true.         !  (default = .true.)
-logical, parameter :: use_extrap_tau_log = .false.    !  (default = .false.)
-logical, parameter :: use_extrap_tau_simple = .true.  !  (default = .true.)
-logical, parameter :: use_modify_dutdn = .false.  !--only works w/interp_tau; not MPI compliant
+!logical, parameter :: vel_BC = .false. 
+logical :: vel_BC = .false. !--means we are forcing velocity for
+                            !  level set BC  
+                            !  (default = .false.)
+! logical, parameter :: use_log_profile = .false.       !  (default = .false.)
+! logical, parameter :: use_enforce_un = .false.        !  (default = .false.)
+! logical, parameter :: physBC = .true.                 !  (default = .true.)        
+! logical, parameter :: use_smooth_tau = .true.         !  (default = .true.)
+! logical, parameter :: use_extrap_tau_log = .false.    !  (default = .false.)
+! logical, parameter :: use_extrap_tau_simple = .true.  !  (default = .true.)
+! logical, parameter :: use_modify_dutdn = .false.  !--only works w/interp_tau; not MPI compliant
+!                                                   !--wont work w/extra_tau_log
+!                                                   !  (default = .false.)
+logical :: use_log_profile = .false.       !  (default = .false.)
+logical :: use_enforce_un = .false.        !  (default = .false.)
+logical :: physBC = .true.                 !  (default = .true.)        
+logical :: use_smooth_tau = .true.         !  (default = .true.)
+logical :: use_extrap_tau_log = .false.    !  (default = .false.)
+logical :: use_extrap_tau_simple = .true.  !  (default = .true.)
+logical :: use_modify_dutdn = .false.  !--only works w/interp_tau; not MPI compliant
                                                   !--wont work w/extra_tau_log
                                                   !  (default = .false.)
 
+! ! Enables scale dependent Cs evaluations (not dynamic evaluation)
+! ! Used when sgs_model=4 in param module
+! logical, parameter :: lag_dyn_modify_beta = .true.
+
 ! Enables scale dependent Cs evaluations (not dynamic evaluation)
 ! Used when sgs_model=4 in param module
-logical, parameter :: lag_dyn_modify_beta = .true.
+logical :: lag_dyn_modify_beta = .true.
+
+! ! Configures the mode in which SOR smoothing is applied in the IB
+! ! 'xy' may be safely used in most cases (must be used for MPI cases)
+! ! '3d' not MPI compliant
+! character (*), parameter :: smooth_mode = 'xy'  !--'xy', '3d'
 
 ! Configures the mode in which SOR smoothing is applied in the IB
 ! 'xy' may be safely used in most cases (must be used for MPI cases)
 ! '3d' not MPI compliant
-character (*), parameter :: smooth_mode = 'xy'  !--'xy', '3d'
+character(25) :: smooth_mode = 'xy'  !--'xy', '3d'
 
-real (rp), parameter :: z0 = 0.0001_rp !--nondimensional roughness length of surface
-!real (rp), parameter :: z0 = 1.6e-3_rp ! rough
+!real (rp), parameter :: zo_level_set = 0.0001_rp !--nondimensional roughness length of surface
+real (rp) :: zo_level_set = 0.0001_rp !--nondimensional roughness length of surface
 
 logical :: phi_cutoff_is_set = .false.
 logical :: phi_0_is_set = .false.
