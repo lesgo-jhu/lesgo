30eca1b71351e2b85a37d311d5f6af31c169c52f
jgraham <jgraham@4c8967c9-1b3f-4110-b94f-c20ed91a0040>
2011-12-01 02:24:11 +0000

    Updated param to have defaults for the variables being read in.

--- param.f90
+++ param.f90
@@ -37,15 +37,17 @@ module param
   character (*), parameter :: path = './'
 
   !--this stuff must be defined, even if not using MPI
-  character (8) :: chcoord  !--holds character representation of coord
+  ! Setting defaults for ones that can be used even with no MPI
   integer :: nproc = 1 !--this must be 1 if no MPI
+  integer :: rank = 0   !--init to 0 (so its defined, even if no MPI)
+  integer :: coord = 0  !--same here
+
+  character (8) :: chcoord  !--holds character representation of coord
   integer :: ierr
   integer :: comm
   integer :: up, down
   integer :: global_rank
   integer :: MPI_RPREC, MPI_CPREC
-  integer :: rank = 0   !--init to bogus (so its defined, even if no MPI)
-  integer :: coord = 0  !--same here
   integer, allocatable, dimension(:) ::  rank_of_coord, coord_of_rank
   integer :: jzmin, jzmax  ! levels that "belong" to this processor, set w/ grid
   !--end mpi stuff
@@ -61,18 +63,18 @@ module param
   real(rprec),parameter::pi=3.1415926535897932384626433_rprec
 
   !integer,parameter:: nx=64,ny=64,nz=(64)/nproc + 1 
-  integer :: Nx, Ny, Nz
+  integer :: Nx=64, Ny=64, Nz=64
   
   !integer, parameter :: nz_tot = (nz - 1) * nproc + 1
   !integer,parameter:: nx2=3*nx/2,ny2=3*ny/2
   !integer,parameter:: lh=nx/2+1,ld=2*lh,lh_big=nx2/2+1,ld_big=2*lh_big
-  integer :: nz_tot
+  integer :: nz_tot = 64
   integer :: nx2, ny2
   integer :: lh, ld, lh_big, ld_big
 
   ! this value is dimensional [m]:
   !real(rprec),parameter::z_i=1000._rprec   !dimensions in meters, height of BL
-  real(rprec) :: z_i
+  real(rprec) :: z_i = 1000.0_rprec
     
   ! these values should be non-dimensionalized by z_i: 
   ! set as multiple of BL height (z_i) then non-dimensionalized by z_i
@@ -81,7 +83,7 @@ module param
   !real(rprec),parameter::L_z= 1.0_rprec
   !real(rprec),parameter::L_y=ny*L_x/nx               ! ensure dy=dx
   !real(rprec),parameter::L_z=(nz_tot - 1)*L_x/nx  ! ensure dz = dx
-  real(rprec) :: L_x, L_y, L_z
+  real(rprec) :: L_x = 2.0*pi, L_y=2.0*pi, L_z=1.0_rprec
 
   ! these values are also non-dimensionalized by z_i:
   !real(rprec),parameter::dz=L_z/(nz_tot-1.) ! or (L_z/nproc)/(nz - 1)
@@ -95,32 +97,32 @@ module param
   ! Model type: 1->Smagorinsky; 2->Dynamic; 3->Scale dependent
   !             4->Lagrangian scale-sim   5-> Lagragian scale-dep
   !integer,parameter::model=5, wall_damp_exp=2
-  integer :: sgs_model, wall_damp_exp
+  integer :: sgs_model=5, wall_damp_exp=2
 
   ! timesteps between dynamic Cs updates           
   !integer, parameter :: cs_count = 5
-  integer :: cs_count
+  integer :: cs_count = 5
 
   ! When to start dynamic Cs calculations
   !integer,parameter::DYN_init=100
-  integer :: DYN_init
+  integer :: DYN_init = 100
   
   ! Cs is the Smagorinsky Constant
   ! Co and wall_damp_exp are used in the mason model for smagorisky coeff
   !real(kind=rprec),parameter::Co=0.16_rprec
-  real(rprec) :: Co
+  real(rprec) :: Co = 0.16_rprec
   
   ! test filter type: 1->cut off 2->Gaussian 3->Top-hat
   !integer,parameter::ifilter=1
-  integer :: ifilter
+  integer :: ifilter = 1
 
   ! u_star=0.45 m/s if coriolis_forcing=.FALSE. and =ug if coriolis_forcing=.TRUE.
   !real(rprec),parameter::u_star=0.45_rprec,Pr=.4_rprec
-  real(rprec) :: u_star
+  real(rprec) :: u_star = 0.45_rprec
 
   ! von Karman constant     
   !real(rprec),parameter::vonk=0.4_rprec   
-  real(rprec) :: vonk
+  real(rprec) :: vonk = 0.4_rprec
   
   ! Coriolis stuff
   ! coriol=non-dim coriolis parameter,
@@ -128,34 +130,34 @@ module param
   !logical,parameter::coriolis_forcing=.false.
   !real(rprec),parameter::coriol=9.125E-05*z_i/u_star,      &
   !     ug=u_star/u_star,vg=0._rprec/u_star
-  logical :: coriolis_forcing
-  real(rprec) :: coriol, ug, vg
+  logical :: coriolis_forcing = .true. 
+  real(rprec) :: coriol = 1.0e-4_rprec, ug=1.0_rprec, vg=0.0_rprec
 
   ! nu_molec is dimensional m^2/s
   !real(rprec),parameter::nu_molec=1.14e-5_rprec   
-  real(rprec) :: nu_molec
+  real(rprec) :: nu_molec = 1.14e-5_rprec
     
   !logical,parameter::molec=.false.,sgs=.true.,dns_bc=.false.  
-  logical :: molec, sgs, dns_bc
+  logical :: molec=.false., sgs=.true., dns_bc=.false.
   
 !---------------------------------------------------
 ! TIMESTEP PARAMETERS
 !---------------------------------------------------   
 
   !integer, parameter :: nsteps = 1500
-  integer :: nsteps
+  integer :: nsteps = 50000
  
   $if($CFL_DT)
   
   !real(rprec), parameter :: cfl = 0.05
-  real(rprec) :: cfl
+  real(rprec) :: cfl = 0.05
   real(rprec) :: dt_f, cfl_f
 
   $endif
   
   !real (rprec), parameter :: dt = 2.0e-4_rprec              ! dt=2.e-4 usually works for 64^3
   !real (rprec), parameter :: dt_dim = dt*z_i/u_star     ! dimensional time step in seconds
-  real(rprec) :: dt
+  real(rprec) :: dt = 2.0e-4_rprec
   real(rprec) :: dt_dim
   
   ! time advance parameters (Adams-Bashforth, 2nd order accurate)
@@ -163,7 +165,7 @@ module param
   real(rprec) :: tadv1, tadv2
   
   !logical, parameter :: cumulative_time = .false.        ! to use total_time.dat
-  logical :: cumulative_time
+  logical :: cumulative_time = .true.
   character (*), parameter :: fcumulative_time = path // 'total_time.dat'
   
   integer :: jt                 ! global time-step counter
@@ -176,47 +178,47 @@ module param
 
   ! initu = true to read from a file; false to create with random noise
   !logical, parameter :: initu = .false.
-  logical :: initu
+  logical :: initu = .false.
   ! initlag = true to initialize cs, FLM & FMM; false to read from vel.out
   !logical, parameter :: inilag = .true.
-  logical :: inilag
+  logical :: inilag = .true.
 
   ! ubc: upper boundary condition: ubc=0 stress free lid
   !integer,parameter::ubc=0
-  integer :: ubc
+  integer :: ubc = 0
   ! lbc: lower boundary condition:  'wall', 'stress free'
   !character (*), parameter :: lbc_mom = 'wall'
-  character(50) :: lbc_mom
+  character(50) :: lbc_mom = 'wall'
   
   ! lower boundary condition, roughness length
   !real (rprec), parameter :: zo = 0.0001_rprec  ! nondimensional  
-  real(rprec) :: zo
+  real(rprec) :: zo = 0.0001_rprec
 
   ! prescribed inflow:   
   !logical,parameter::inflow=.false.
-  logical :: inflow
+  logical :: inflow = .false.
   ! if inflow is true the following should be set:
     ! position of right end of fringe region, as a fraction of L_x
     !real (rprec), parameter :: fringe_region_end = 1._rprec
-    real(rprec) :: fringe_region_end
+    real(rprec) :: fringe_region_end  = 1.0_rprec
     ! length of fringe region as a fraction of L_x
     !real (rprec), parameter :: fringe_region_len = 0.125_rprec  
-    real(rprec) :: fringe_region_len
+    real(rprec) :: fringe_region_len = 0.125_rprec
 
     ! Use uniform inflow instead of concurrent precursor inflow
     !logical, parameter :: uniform_inflow = .false.
-    logical :: uniform_inflow
+    logical :: uniform_inflow = .false.
       !real (rprec), parameter :: inflow_velocity = 1.0_rprec
-      real(rprec) :: inflow_velocity
+      real(rprec) :: inflow_velocity = 1.0_rprec
       ! velocities are forced to the inflow velocity
       !logical, parameter :: force_top_bot = .false.
-      logical :: force_top_bot
+      logical :: force_top_bot = .false.
 
   ! if true, imposes a pressure gradient in the x-direction to force the flow
   !logical, parameter :: use_mean_p_force = .true.
-  logical :: use_mean_p_force
+  logical :: use_mean_p_force = .true.
   !real (rprec), parameter :: mean_p_force = 1._rprec / L_z
-  real(rprec) :: mean_p_force
+  real(rprec) :: mean_p_force = 1.0_rprec
   
 !---------------------------------------------------
 ! DATA OUTPUT PARAMETERS
@@ -224,22 +226,22 @@ module param
 
   ! how often to display "jt,dt,rmsdivvel,ke,cfl" output
   !integer,parameter::wbase=100
-  integer :: wbase
+  integer :: wbase = 100
   
   ! how often to write ke to check_ke.out
   !integer, parameter :: nenergy = 100
-  integer :: nenergy
+  integer :: nenergy = 100
 
   ! how often to display Lagrangian CFL condition of 
   ! dynamic SGS models
   !integer,parameter::lag_cfl_count=1000
-  integer :: lag_cfl_count
+  integer :: lag_cfl_count = 1000
 
   ! records time-averaged data to files ./output/*_avg.dat
   !logical, parameter :: tavg_calc = .true.
   !integer, parameter :: tavg_nstart = 50000, tavg_nend = nsteps
-  logical :: tavg_calc
-  integer :: tavg_nstart, tavg_nend
+  logical :: tavg_calc = .false.
+  integer :: tavg_nstart = 1, tavg_nend = 50000
 
   ! turns instantaneous velocity recording on or off
   !logical, parameter :: point_calc = .true.
@@ -248,25 +250,25 @@ module param
   !type(point3D), parameter, dimension(point_nloc) :: point_loc = (/ &
   !      point3D( (/ L_x / 2, L_y / 2, L_z / 2 /) ) &
   !      /)
-  logical :: point_calc
-  integer :: point_nstart, point_nend, point_nskip
-  integer :: point_nloc
+  logical :: point_calc = .false.
+  integer :: point_nstart=1, point_nend=50000, point_nskip=10
+  integer :: point_nloc=1
   type(point3D), allocatable, dimension(:) :: point_loc
 
   ! domain instantaneous output
   !logical, parameter :: domain_calc = .true.
   !integer, parameter :: domain_nstart = 50000, domain_nend = nsteps, domain_nskip = 50000
-  logical :: domain_calc
-  integer :: domain_nstart, domain_nend, domain_nskip
+  logical :: domain_calc=.false.
+  integer :: domain_nstart=10000, domain_nend=50000, domain_nskip=10000
   
   ! x-plane instantaneous output
   !logical, parameter :: xplane_calc   = .true.
   !integer, parameter :: xplane_nstart = 50000, xplane_nend = nsteps, xplane_nskip  = 10000
   !integer, parameter :: xplane_nloc   = 2
   !real(rprec), parameter, dimension(xplane_nloc) :: xplane_loc = (/ L_x/4._rprec, L_x/2._rprec /)
-  logical :: xplane_calc
-  integer :: xplane_nstart, xplane_nend, xplane_nskip
-  integer :: xplane_nloc
+  logical :: xplane_calc=.false.
+  integer :: xplane_nstart=10000, xplane_nend=50000, xplane_nskip=10000
+  integer :: xplane_nloc=1
   real(rprec), allocatable, dimension(:) :: xplane_loc
 
   ! y-plane instantaneous output
@@ -274,9 +276,9 @@ module param
   ! integer, parameter :: yplane_nstart = 50000, yplane_nend = nsteps, yplane_nskip  = 50000
   ! integer, parameter :: yplane_nloc   = 2
   ! real(rprec), parameter, dimension(yplane_nloc) :: yplane_loc = (/ L_y/4._rprec, L_y/2._rprec  /)  
-  logical :: yplane_calc
-  integer :: yplane_nstart, yplane_nend, yplane_nskip
-  integer :: yplane_nloc
+  logical :: yplane_calc=.false.
+  integer :: yplane_nstart=10000, yplane_nend=50000, yplane_nskip=10000
+  integer :: yplane_nloc=1
   real(rprec), allocatable, dimension(:) :: yplane_loc
 
   ! z-plane instantaneous output
@@ -284,19 +286,18 @@ module param
   ! integer, parameter :: zplane_nstart = 50000, zplane_nend = nsteps, zplane_nskip  = 10000
   ! integer, parameter :: zplane_nloc   = 3
   ! real(rprec), parameter, dimension(zplane_nloc) :: zplane_loc = (/ 0.1_rprec, 0.25_rprec, 0.5_rprec /)
-  logical :: zplane_calc
-  integer :: zplane_nstart, zplane_nend, zplane_nskip
-  integer :: zplane_nloc
+  logical :: zplane_calc=.false.
+  integer :: zplane_nstart=10000, zplane_nend=50000, zplane_nskip=10000
+  integer :: zplane_nloc=1
   real(rprec), allocatable, dimension(:) :: zplane_loc
 
   ! logical, parameter :: spectra_calc = .true.
   ! integer, parameter :: spectra_nstart = 50000, spectra_nend = nsteps
   ! integer, parameter :: spectra_nloc = 3
   ! real(rprec), parameter, dimension(spectra_nloc) :: spectra_loc = (/ 0.1_rprec, 0.25_rprec, 0.5_rprec /) 
-  logical :: spectra_calc
-  integer :: spectra_nstart, spectra_nend
-  integer :: spectra_nloc
+  logical :: spectra_calc=.false.
+  integer :: spectra_nstart=10000, spectra_nend=50000
+  integer :: spectra_nloc=1
   real(rprec), allocatable, dimension(:) :: spectra_loc
-
 
 end module param
