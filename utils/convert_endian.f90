1c4a621cd9ef748095c09dd46dcaeddacf48df59
jgraham <jgraham@4c8967c9-1b3f-4110-b94f-c20ed91a0040>
2011-12-08 23:20:43 +0000

    Added ability to work with integer data; updated help menu

--- utils/convert_endian.f90
+++ utils/convert_endian.f90
@@ -3,13 +3,18 @@ implicit none
 integer, parameter :: rp = kind (1.d0)
 
 character(128) :: fbase
+
 logical :: backup=.false.
+logical :: integer_data=.false.
+
+! Not implemented yet
+logical :: streamio = .false.
 
 ! 1 - little to big endian; 2 - big to little endian
 integer :: iendian = 1
 
 ! Exact size of the variables and number of variables
-integer :: nx=32, ny=32 , nz=32, nvar=3
+integer :: nx=1, ny=1 , nz=1, nvar=1
 
 end module convert_endian_base
 
@@ -59,48 +64,41 @@ subroutine print_help_message()
 !     Jason Graham <jgraha8@gmail.com>
 !
 write(*,*) 'Usage:'
-write(*,*) '  convert-endian [options] -e {1|2} -nx <nx> -ny <ny> -nz <nz> -n <n> file'
+write(*,*) '  convert-endian [options] -e {1|2} -n <elements> file'
 write(*,*) ' '
 write(*,*) 'Options:'
-write(*,*) '  -b                       Backup input file'
+write(*,*) '  -B                       Backup input file (off by default)'
+write(*,*) '  -i                       Specifies that data is of type integer (real default)'
+write(*,*) '  -nx <nx>                 Set the number of dimensions in x-direction (default 1)'
+write(*,*) '  -ny <ny>                 Set the number of dimensions in y-direction (default 1)'
+write(*,*) '  -nz <nz>                 Set the number of dimensions in z-direction (default 1)'
 write(*,*) ' '
 write(*,*) 'Arguments:'
-write(*,*) '  -e                       Endian conversion flag: 1 - little to endian; 2 - endian to little'
-write(*,*) '  -nx                      Set the number of dimensions in x-direction'
-write(*,*) '  -ny                      Set the number of dimensions in y-direction'
-write(*,*) '  -nz                      Set the number of dimensions in z-direction'
-write(*,*) '  -n                       Set the number of variables'
+write(*,*) '  -e                       Endian conversion flag: 1 - little to big; 2 - big to little'
+write(*,*) '  -n                       Set the number of data elements (or number of variables if '
+write(*,*) '                           nx, ny, and/or nz is specified)'
 write(*,*) ' '
 write(*,*) 'NOTE: the order of arguments are arbitrary'
 end subroutine print_help_message
 
 end module messages
 
+!***********************************************************************
 program convert_endian
+!***********************************************************************
 use convert_endian_base
 implicit none
 
-!character (*), parameter :: path='./output'
-!character (*), parameter :: fbase = path // 'uvw.$FITER.out'
-!character (*), parameter :: MPI_suffix = '.c'  !--must be proc number
-
-!logical, parameter :: MPI = .true.
-
-
-character (64) :: fmt
 character (128) :: fname
-character (64) :: read_endian, write_endian
-
-integer :: ip
-integer :: lbz, ubz
-integer :: i, j, k
-
-logical :: fexist
+character (64) :: read_endian, write_endian, data_access
 
 ! Array which holds the data
-real(rp), allocatable, dimension(:,:,:,:) :: vars
+real(rp), target, allocatable, dimension(:,:,:,:) :: vars
 
+
+integer, target, allocatable, dimension(:,:,:,:) :: ivars
 !---------------------------------------------------------------------
+
 
 !//////////////////////////////////////
 !/// READ DATA                      ///
@@ -108,8 +106,12 @@ real(rp), allocatable, dimension(:,:,:,:) :: vars
 call read_input_arguments()
 !write(*,*) 'nx, ny, nz, nvar : ', nx, ny, nz, nvar
 ! Allocate space
-allocate(vars(nx,ny,nz,nvar))
-vars=0.0
+if( integer_data ) then
+   allocate(ivars(nx,ny,nz,nvar))
+else
+   allocate(vars(nx,ny,nz,nvar))
+   vars=huge(1.0_rp)
+endif
 
 if(iendian == 1) then
   read_endian = 'little_endian'
@@ -122,10 +124,21 @@ else
   stop
 endif
 
+if(streamio) then
+   data_access='stream'
+else
+   data_access='sequential'
+endif
+
 ! Load the data
 write(*,*) 'Converting file : ', trim(adjustl(fbase))
-open (1, file=fbase, form='unformatted', convert=trim(adjustl(read_endian)))
-read(1) vars
+open (1, file=fbase, form='unformatted', convert=trim(adjustl(read_endian)), &
+     position='rewind', access=data_access)
+if( integer_data ) then
+   read(1) ivars
+else
+   read(1) vars
+endif
 close (1)
 
 if( backup ) then
@@ -137,8 +150,15 @@ endif
 
 ! Now over write the original file
 write(*,*) 'Converting from ' // trim(adjustl(read_endian)) // ' to ' // trim(adjustl(write_endian))
-open (1, file=fbase, form='unformatted', convert=trim(adjustl(write_endian)))
-write (1) vars
+open (1, file=fbase, form='unformatted', convert=trim(adjustl(write_endian)), &
+     position='rewind', access=data_access)
+
+if( integer_data ) then
+   write(1) ivars
+else
+   write(1) vars
+endif
+
 close (1)
 
 write(*,*) 'Conversion complete'
@@ -183,9 +203,13 @@ input: do while (n < narg)
 
   n=n+1
 
-  if(arg(n) == '-b') then
+  if(arg(n) == '-B') then
 
      backup = .true.
+
+  elseif( arg(n) == '-i') then
+     
+     integer_data = .true.
 
   elseif(arg(n) == '-e') then
 
