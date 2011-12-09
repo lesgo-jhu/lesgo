59eac8bcb3b64d1291ccd0a84316ec76b9f32e40
jgraham <jgraham@4c8967c9-1b3f-4110-b94f-c20ed91a0040>
2011-12-09 01:32:56 +0000

    Added support for handling multiple records in the file. Still assumes that each record is of the same data type.

--- utils/convert_endian.f90
+++ utils/convert_endian.f90
@@ -5,7 +5,7 @@ integer, parameter :: rp = kind (1.d0)
 character(128) :: fbase
 
 logical :: backup=.false.
-logical :: integer_data=.false.
+logical :: integer_flag=.false.
 
 ! Not implemented yet
 logical :: streamio = .false.
@@ -14,7 +14,21 @@ logical :: streamio = .false.
 integer :: iendian = 1
 
 ! Exact size of the variables and number of variables
-integer :: nx=1, ny=1 , nz=1, nvar=1
+integer :: nx=1, ny=1 , nz=1, nelem=1
+
+! Number of records in the file
+character(128) :: frecords
+logical :: multiple_records = .false.
+integer :: nrecords=1
+
+type records
+   logical :: integer_flag
+   integer :: nelem
+   integer, pointer, dimension(:) :: ivars
+   real(rp), pointer, dimension(:) :: vars   
+end type records
+
+type( records ), allocatable, dimension(:) :: records_t
 
 end module convert_endian_base
 
@@ -72,6 +86,8 @@ write(*,*) '  -i                       Specifies that data is of type integer (r
 write(*,*) '  -nx <nx>                 Set the number of dimensions in x-direction (default 1)'
 write(*,*) '  -ny <ny>                 Set the number of dimensions in y-direction (default 1)'
 write(*,*) '  -nz <nz>                 Set the number of dimensions in z-direction (default 1)'
+write(*,*) '  -f <recordsfile>         Specify input records file (required when multiple records'
+write(*,*) '                           are in the input file)'
 write(*,*) ' '
 write(*,*) 'Arguments:'
 write(*,*) '  -e                       Endian conversion flag: 1 - little to big; 2 - big to little'
@@ -92,25 +108,36 @@ implicit none
 character (128) :: fname
 character (64) :: read_endian, write_endian, data_access
 
-! Array which holds the data
-real(rp), target, allocatable, dimension(:,:,:,:) :: vars
-
-
-integer, target, allocatable, dimension(:,:,:,:) :: ivars
+integer :: n
 !---------------------------------------------------------------------
 
 
 !//////////////////////////////////////
 !/// READ DATA                      ///
 !//////////////////////////////////////
+! Load global definitions
 call read_input_arguments()
-!write(*,*) 'nx, ny, nz, nvar : ', nx, ny, nz, nvar
-! Allocate space
-if( integer_data ) then
-   allocate(ivars(nx,ny,nz,nvar))
+
+! Initialize the data structures
+if( multiple_records) then
+
+   call read_records_file()
+
 else
-   allocate(vars(nx,ny,nz,nvar))
-   vars=huge(1.0_rp)
+
+   allocate( records_t( 1 ) )
+
+   records_t(1) % integer_flag = integer_flag
+   records_t(1) % nelem = nx*ny*nz*nelem
+   
+   if( integer_flag ) then
+      allocate(records_t(1) % ivars( records_t(1) % nelem ))
+      records_t(1) % ivars = huge(1)
+   else
+      allocate(records_t(1) % vars( records_t(1) % nelem ))
+      records_t(1) % vars=huge(1.0_rp)
+   endif
+ 
 endif
 
 if(iendian == 1) then
@@ -130,16 +157,6 @@ else
    data_access='sequential'
 endif
 
-! Load the data
-write(*,*) 'Converting file : ', trim(adjustl(fbase))
-open (1, file=fbase, form='unformatted', convert=trim(adjustl(read_endian)), &
-     position='rewind', access=data_access)
-if( integer_data ) then
-   read(1) ivars
-else
-   read(1) vars
-endif
-close (1)
 
 if( backup ) then
    ! Save a copy
@@ -148,17 +165,38 @@ if( backup ) then
    call system('cp ' // trim(adjustl(fbase)) // ' ' // trim(adjustl(fname))) 
 endif
 
-! Now over write the original file
-write(*,*) 'Converting from ' // trim(adjustl(read_endian)) // ' to ' // trim(adjustl(write_endian))
-open (1, file=fbase, form='unformatted', convert=trim(adjustl(write_endian)), &
+! Load the data
+write(*,*) 'Converting file : ', trim(adjustl(fbase))
+open (1, file=fbase, form='unformatted', convert=trim(adjustl(read_endian)), &
      position='rewind', access=data_access)
 
-if( integer_data ) then
-   write(1) ivars
-else
-   write(1) vars
-endif
+do n = 1, nrecords
 
+   if( records_t(n) % integer_flag ) then
+      read(1) records_t(n) % ivars
+   else
+      read(1) records_t(n) % vars
+   endif
+
+enddo
+
+! Rewind file to beginning
+REWIND(1)
+
+! Now over write the original file
+write(*,*) 'Converting from ' // trim(adjustl(read_endian)) // ' to ' // trim(adjustl(write_endian))
+!open (1, file=fbase, form='unformatted', convert=trim(adjustl(write_endian)), &
+!     position='rewind', access=data_access)
+
+do n = 1, nrecords
+
+   if( records_t(n) % integer_flag ) then
+      write(1) records_t(n) % ivars
+   else
+      write(1) records_t(n) % vars
+   endif
+
+enddo
 close (1)
 
 write(*,*) 'Conversion complete'
@@ -209,14 +247,7 @@ input: do while (n < narg)
 
   elseif( arg(n) == '-i') then
      
-     integer_data = .true.
-
-  elseif(arg(n) == '-e') then
-
-     n=n+1
-     ! Check that argument specified
-     if( n > narg ) call print_error_message('endian conversion flag not specified')
-     read(arg(n),*) iendian
+     integer_flag = .true.
 
   elseif( arg(n) == '-nx' ) then 
 
@@ -239,12 +270,30 @@ input: do while (n < narg)
      if( n > narg ) call print_error_message('nz not specified')
      read(arg(n),*) nz
 
+  elseif(arg(n) == '-f') then
+
+     multiple_records =  .true.
+
+     n=n+1
+     ! Check that argument specified
+     if( n > narg ) call print_error_message('records file not specified')
+     read(arg(n),*) frecords
+
+  elseif(arg(n) == '-e') then
+
+     n=n+1
+     ! Check that argument specified
+     if( n > narg ) call print_error_message('endian conversion flag not specified')
+     read(arg(n),*) iendian
+
+
+
   elseif( arg(n) == '-n' ) then 
 
      n=n+1
      ! Check that argument specified
-     if( n > narg ) call print_error_message('nvar not specified')
-     read(arg(n),*) nvar
+     if( n > narg ) call print_error_message('number of elements (or variables)  not specified')
+     read(arg(n),*) nelem
 
 
   elseif(arg(n) == '-h' .or. arg(n) == '--help') then
@@ -266,5 +315,49 @@ if( .not. exst ) call print_error_message('input file does not exist')
 return
 end subroutine read_input_arguments
 
+!***********************************************************************
+subroutine read_records_file()
+!***********************************************************************
+use convert_endian_base, only : rp, records_t, nrecords, frecords
+implicit none
 
+integer :: n, nelem
+logical :: integer_flag
+
+integer :: istat
+
+open(101,file=frecords,position='rewind')
+
+nrecords=0
+istat=0
+do while (istat == 0)
+
+  read(101,*,IOSTAT=istat) integer_flag, nelem
+  if( istat == 0 ) nrecords = nrecords + 1
+  
+enddo
+
+! Start back at beginning
+rewind(101)
+
+! Now allocate records
+allocate( records_t( nrecords ) ) 
+do n=1, nrecords
+
+   read(101,*) integer_flag, nelem
+
+   records_t( n ) % integer_flag = integer_flag
+   records_t( n ) % nelem = nelem
+
+   if( integer_flag ) then
+      allocate( records_t( n ) % ivars( nelem ) )
+      records_t(n) % ivars = huge(1)
+   else
+      allocate( records_t( n ) % vars( nelem ) )
+      records_t( n ) % vars = huge(1.0_rp) 
+   endif
+enddo
+
+return
+end subroutine read_records_file
 
