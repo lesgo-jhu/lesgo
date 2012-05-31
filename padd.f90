91c0934beb6e8b0d5c6b56b2311b2a2bcfc22f00
jgraham <jgraham@4c8967c9-1b3f-4110-b94f-c20ed91a0040>
2012-05-31 01:38:06 +0000

    Removed loops since just doing simple stuff and there are no conditionals and is independent. Now using explicit access for arrays which allow similar access patterns as unrolled loops

--- padd.f90
+++ padd.f90
@@ -1,33 +1,31 @@
+!*******************************************************************************
 subroutine padd (u_big,u)
+!*******************************************************************************
 ! puts arrays into larger, zero-padded arrays 
 ! automatically zeroes the oddballs
 use types,only:rprec
 use param,only:ld,ld_big,nx,ny,ny2
 implicit none
-integer::jx,jy
+
 !  u and u_big are interleaved as complex arrays
 real(kind=rprec), dimension(ld,ny), intent(in) :: u
 real(kind=rprec), dimension(ld_big,ny2), intent(out) :: u_big
 
+integer :: ny_h, j_s, j_big_s
+
+ny_h = ny/2
+
 ! make sure the big array is zeroed!
-do jy=1,ny2
-  do jx=1,ld_big
-    u_big(jx,jy) = 0._rprec
-  enddo
-enddo
+u_big(:,:) = 0._rprec
 
-! note: the loops are split in an attempt to maintain locality
-do jy=1,ny/2
-  do jx=1,nx
-    u_big(jx,jy) = u(jx,jy)
-  end do
-end do
+! note: split access in an attempt to maintain locality
+u_big(:nx,:ny_h) = u(:nx,:ny_h)
 
-do jy=1,ny/2-1
-  do jx=1,nx
-    u_big(jx,jy+ny2-ny/2+1) = u(jx,jy+ny/2+1)
-  end do
-end do
+! Compute starting j locations for second transfer
+j_s = ny_h + 2
+j_big_s = ny2 - ny_h + 2
+
+u_big(:nx,j_big_s:ny2) = u(:nx,j_s:ny)
 
 return
 end subroutine padd
