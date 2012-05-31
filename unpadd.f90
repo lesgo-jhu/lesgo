b495d932c96cad9c726bebaa1314fe022b2739d2
jgraham <jgraham@4c8967c9-1b3f-4110-b94f-c20ed91a0040>
2012-05-31 01:47:26 +0000

    Removed loops since just doing simple stuff and there are no conditionals and is independent. Now using explicit access for arrays which allow similar access patterns as unrolled loops

--- unpadd.f90
+++ unpadd.f90
@@ -1,26 +1,27 @@
+!*******************************************************************************
 subroutine unpadd(cc,cc_big)
+!*******************************************************************************
 use types,only:rprec
 use param,only:ld,nx,ny,ny2,nz,ld_big,lh
 implicit none
-integer::jx,jy
+
 !  cc and cc_big are interleaved as complex arrays
 real(rprec), dimension( ld, ny ) :: cc
 real(rprec), dimension( ld_big, ny2 ) :: cc_big
 
-do jy=1,ny/2
-  do jx=1,nx
-   cc(jx,jy) = cc_big(jx,jy)
-  end do
-end do
+integer :: ny_h, j_s, j_big_s
+
+ny_h = ny/2
+
+cc(:nx,:ny_h) = cc_big(:nx,:ny_h)
 
 ! oddballs
 cc(ld-1:ld,:) = 0._rprec
-cc(:,ny/2+1) = 0._rprec
+cc(:,ny_h+1) = 0._rprec
 
-do jy=1,ny/2-1
-  do jx=1,nx
-    cc(jx,jy+ny/2+1) = cc_big(jx,jy+ny2-ny/2+1)
-  end do
-end do
+! Compute starting j locations for second transfer
+j_s = ny_h + 2
+j_big_s = ny2 - ny_h + 2
+cc(:nx,j_s:ny) = cc_big(:nx,j_big_s:ny2)
 
 end subroutine unpadd
