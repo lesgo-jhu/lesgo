7ad0a4074d437a4d5b3e563279cfb9da25b506b2
jgraham <jgraham@4c8967c9-1b3f-4110-b94f-c20ed91a0040>
2012-06-06 20:06:49 +0000

    Converted '3D' to '3d' for aesthetic reasons. Also removed det2D since it is not used.

--- rns_ls.f90
+++ rns_ls.f90
@@ -179,7 +179,7 @@ use types, only : rprec
 use messages
 use sim_param, only : u, v
 use sim_param, only : fx, fy
-use functions, only : points_avg_3D
+use functions, only : points_avg_3d
 use param, only : nx, nz, dx, dy, dz, coord, jt, jt_total
 $if($MPI)
 use mpi
@@ -240,8 +240,8 @@ do n=1, nbeta_elem
   npoint_p => beta_elem_t(n) % ref_region_t % npoint
   points_p => beta_elem_t(n) % ref_region_t % points
   
-  u_p = points_avg_3D( u(1:nx,:,1:nz), 1, npoint_p, points_p ) 
-  v_p = points_avg_3D( v(1:nx,:,1:nz), 1, npoint_p, points_p )
+  u_p = points_avg_3d( u(1:nx,:,1:nz), 1, npoint_p, points_p ) 
+  v_p = points_avg_3d( v(1:nx,:,1:nz), 1, npoint_p, points_p )
   
   cache = 0.5_rprec * sqrt( u_p**2 + v_p**2 ) * area_p
   beta_gamma(:,n) = cache * (/ u_p, v_p /)
@@ -258,8 +258,8 @@ do n=1, nb_elem
   npoint_p => b_elem_t(n) % ref_region_t % npoint
   points_p => b_elem_t(n) % ref_region_t % points
   
-  u_p = points_avg_3D( u(1:nx,:,1:nz), 1, npoint_p, points_p ) 
-  v_p = points_avg_3D( v(1:nx,:,1:nz), 1, npoint_p, points_p )
+  u_p = points_avg_3d( u(1:nx,:,1:nz), 1, npoint_p, points_p ) 
+  v_p = points_avg_3d( v(1:nx,:,1:nz), 1, npoint_p, points_p )
   
   cache = 0.5_rprec * sqrt( u_p**2 + v_p**2 ) * area_p
   b_gamma(:,n) = cache * (/ u_p, v_p /)
@@ -750,7 +750,6 @@ subroutine b_elem_CD_LITW()
 !  Used variable declarations from contained subroutine rns_elem_force_ls
 !
 use param, only : wbase
-use functions, only : det2D
 implicit none
 
 !integer :: i1,i2, info
@@ -1005,7 +1004,7 @@ $if($MPI)
 use param, only : MPI_RPREC, MPI_SUM, comm, ierr
 $endif
 use sim_param, only : u, v
-use functions, only : points_avg_3D
+use functions, only : points_avg_3d
 use sim_param, only : fx, fy
 implicit none
 
@@ -1044,8 +1043,8 @@ do n = 1, nr_elem
 
   !  Get the reference velocity
   ref_region_t_p => r_elem_t( n ) % ref_region_t
-  ref_region_t_p % u = points_avg_3D( u(1:nx,:,1:nz), 1, ref_region_t_p % npoint, ref_region_t_p % points)
-  ref_region_t_p % v = points_avg_3D( v(1:nx,:,1:nz), 1, ref_region_t_p % npoint, ref_region_t_p % points)
+  ref_region_t_p % u = points_avg_3d( u(1:nx,:,1:nz), 1, ref_region_t_p % npoint, ref_region_t_p % points)
+  ref_region_t_p % v = points_avg_3d( v(1:nx,:,1:nz), 1, ref_region_t_p % npoint, ref_region_t_p % points)
   
   indx_array_t_p => r_elem_t( n ) % indx_array_t
      
