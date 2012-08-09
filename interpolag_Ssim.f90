3a7d741e52e128965b534637d2bff809c123fe11
cverhul1 <cverhul1@4c8967c9-1b3f-4110-b94f-c20ed91a0040>
2012-08-09 19:08:34 +0000

    Changed/added a couple comments

--- interpolag_Ssim.f90
+++ interpolag_Ssim.f90
@@ -67,9 +67,9 @@ z => grid_t % z
         $endif 
     
         ! Loop over domain (within proc): for each, calc xyz_past then trilinear_interp
-        ! Variables x,y,z_lag, F_LM, F_MM, etc are on w-grid
+        ! Variables x,y,z, F_LM, F_MM, etc are on w-grid
         ! Interpolation out of top/bottom of domain is not permitted.
-        ! Note: x,y,z_lag values are only good for k=1:nz-1 within each proc
+        ! Note: x,y,z values are only good for k=1:nz-1 within each proc
             if ( coord.eq.0 ) then
                 kmin = 2 
                 ! At the bottom-most level (at the wall) the velocities are zero.
@@ -82,7 +82,7 @@ z => grid_t % z
             do k=kmin,nz-1
             do j=1,ny
             do i=1,nx
-                ! Determine position at previous timestep                   
+                ! Determine position at previous timestep (u,v interp to w-grid) 
                 xyz_past(1) = x(i) - 0.5_rprec*(u(i,j,k-1)+u(i,j,k))*lagran_dt
                 xyz_past(2) = y(j) - 0.5_rprec*(v(i,j,k-1)+v(i,j,k))*lagran_dt
                 xyz_past(3) = z(k) - w(i,j,k)*lagran_dt
@@ -105,7 +105,7 @@ z => grid_t % z
                 k = nz
                 do j=1,ny
                 do i=1,nx
-                    ! Determine position at previous timestep
+                    ! Determine position at previous timestep (u,v interp to w-grid)
                     xyz_past(1) = x(i) - 0.5_rprec*(u(i,j,k-1)+u(i,j,k))*lagran_dt
                     xyz_past(2) = y(j) - 0.5_rprec*(v(i,j,k-1)+v(i,j,k))*lagran_dt  
                     xyz_past(3) = z(k) - max(0.0_rprec,w(i,j,k))*lagran_dt
