126d0751632394e3a1677a901852dc091ea8aaeb
jgraham <jgraham@4c8967c9-1b3f-4110-b94f-c20ed91a0040>
2013-03-03 00:34:00 +0000

    Backported fixes from devel_branch (tag stable_snap_20121024_branch_merge_3)

--- stat_defs.f90
+++ stat_defs.f90
@@ -38,15 +38,13 @@ real(rprec) :: tavg_total_time
 $if($OUTPUT_EXTRA)
 real(rprec) :: tavg_total_time_sgs
 $endif
-! Time stamp of last time averaging computation
-real(rprec) :: tavg_time_stamp  
+! Time between calls of tavg_compute, built by summing dt
+real(rprec) :: tavg_dt
 ! Switch for determining if time averaging has been initialized
 logical :: tavg_initialized = .false.
 
-! Time step used for time averaging of spectra
-real(rprec) :: dt_spectra
-! Time stamp of last spectra computation
-real(rprec) :: spectra_time_stamp
+! Time between calls of spectra_compute, built by summing dt
+real(rprec) :: spectra_dt
 ! Switch for determining if time averaging has been initialized
 logical :: spectra_initialized = .false.
 
@@ -82,11 +80,11 @@ type turbine_t
   real(rprec) :: theta2                       ! angle above the horizontal, from -x dir [degrees]
   real(rprec), dimension(3) :: nhat           ! (nx,ny,nz) of unit normal for each turbine
   integer :: num_nodes                        ! number of nodes associated with each turbine
-  integer, dimension(1500,3) :: nodes         ! (i,j,k) of each included node
+  integer, dimension(5000,3) :: nodes         ! (i,j,k) of each included node
   integer, dimension(6) :: nodes_max          ! search area for nearby nodes
   real(rprec) :: u_d, u_d_T                   ! running time-average of mean disk velocity
   real(rprec) :: f_n                          ! normal force on turbine disk
-  real(rprec), dimension(1500) :: ind         ! indicator function - weighting of each node
+  real(rprec), dimension(5000) :: ind         ! indicator function - weighting of each node
 end type turbine_t
 
 ! A collection of wind-turbines
