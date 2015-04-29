de1fb4f1a18c72c749dbe9c49a46c9844b2bc7c8
Joel Bretheim <jbretheim@gmail.com>
2015-04-29 15:34:46 -0400

    added comments to launch script

--- skel/njord-launch.sh
+++ skel/njord-launch.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 
 # For launching lesgo jobs on njord cluster.
-# Searches lesgo configuration file for number of processors to be used then launches mpi job.
 # Author: Joel Bretheim, jbretheim@gmail.com
 
+# specify the configuration file
 conf=lesgo.conf
 
+# grab number of processors from conf
 nproc=`grep "nproc" $conf | awk '{print $3}'`
-
 printf "Found nproc = $nproc in $conf \n"
 
-mpirun -np $nproc ./lesgo-mpi-* > lesgo.out &
+# launch job
+mpirun -np $nproc ./lesgo-mpi* > lesgo.out &
