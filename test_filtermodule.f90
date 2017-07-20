fc017f0e298fae25c9357ac69c90397ed5766a64
Carl Shapiro <crshapiro@gmail.com>
2017-07-20 09:27:18 -0400

    cleanup test_filtermodule.f90.

--- test_filtermodule.f90
+++ test_filtermodule.f90
@@ -1,6 +1,6 @@
 !!
-!!  Copyright (C) 2009-2013  Johns Hopkins University
-!!
+!!  Copyright (C) 2009-2017  Johns Hopkins University
+!7
 !!  This file is part of lesgo.
 !!
 !!  lesgo is free software: you can redistribute it and/or modify
@@ -17,145 +17,155 @@
 !!  along with lesgo.  If not, see <http://www.gnu.org/licenses/>.
 !!
 
+!*******************************************************************************
 module test_filtermodule
-use types,only:rprec
-use param,only:lh,ny
+!*******************************************************************************
+use types, only : rprec
+use param, only : lh, ny
 
-private lh,ny
+private lh, ny
 
-    integer, parameter::filter_size=1   ! the implicit filter (1=grid size)
-    real(rprec) :: alpha_test = 2.0_rprec * filter_size ! alpha is ratio of test filter to grid filter widths
-    real(rprec) :: alpha_test_test = 4.0_rprec * filter_size
-
-    real(rprec), dimension(:,:), allocatable :: G_test, G_test_test
+! the implicit filter (1=grid size)
+integer, parameter :: filter_size=1
+! alpha is ratio of test filter to grid filter widths
+real(rprec) :: alpha_test = 2.0_rprec * filter_size 
+real(rprec) :: alpha_test_test = 4.0_rprec * filter_size
+real(rprec), dimension(:,:), allocatable :: G_test, G_test_test
 
 contains
 
-!**********************************************************************
+!*******************************************************************************
 subroutine test_filter_init()
-!**********************************************************************
+!*******************************************************************************
 ! Creates the kernels which will be used for filtering the field
-
-use types,only:rprec
-use param,only:lh,nx,ny,dx,dy,pi,ifilter,sgs_model
+use types, only : rprec
+use param, only : lh, nx, ny, dx, dy, pi, ifilter, sgs_model
 use fft
-
 implicit none
 
-real(rprec):: delta_test, kc2_test, delta_test_test, kc2_test_test
+real(rprec) :: delta_test, kc2_test, delta_test_test, kc2_test_test
 
 ! Allocate the arrays
-    allocate ( G_test(lh,ny) )
+allocate( G_test(lh,ny) )
 
-! Include the normalization
-    G_test = 1._rprec/(nx*ny)  ! normalization for the forward FFT
+! Include the normalization for the forward FFT
+G_test = 1._rprec/(nx*ny)
 
 ! Filter characteristic width
-    delta_test = alpha_test * sqrt(dx*dy)  ! "2d-delta", not full 3d one
+! "2d-delta", not full 3d one
+delta_test = alpha_test * sqrt(dx*dy) 
 
 ! Calculate the kernel
-    if(ifilter==1) then ! spectral cutoff filter
-       if (sgs_model==6.OR.sgs_model==7) then
-          print *, 'Use Gaussian or Top-hat filter for mixed models'
-          stop
-       endif
-       kc2_test = (pi/(delta_test))**2
-       where (real(k2, rprec) >= kc2_test) G_test = 0._rprec
-    else if(ifilter==2) then ! Gaussian filter
-       G_test=exp(-(delta_test)**2*k2/(4._rprec*6._rprec))*G_test   
-    else if(ifilter==3) then ! Top-hat (Box) filter
-       G_test= (sin(kx*delta_test/2._rprec)*sin(ky*delta_test/2._rprec)+1E-8)/&
-            (kx*delta_test/2._rprec*ky*delta_test/2._rprec+1E-8)*G_test
+! spectral cutoff filter
+if(ifilter==1) then
+    if (sgs_model==6.OR.sgs_model==7) then
+        print *, 'Use Gaussian or Top-hat filter for mixed models'
+        stop
     endif
+    kc2_test = (pi/(delta_test))**2
+    where (real(k2, rprec) >= kc2_test) G_test = 0._rprec
+    
+! Gaussian filter
+else if(ifilter==2) then 
+    G_test=exp(-(delta_test)**2*k2/(4._rprec*6._rprec))*G_test   
+
+! Top-hat (Box) filter
+else if(ifilter==3) then 
+    G_test = (sin(kx*delta_test/2._rprec)*sin(ky*delta_test/2._rprec)+1E-8)/   &
+        (kx*delta_test/2._rprec*ky*delta_test/2._rprec+1E-8)*G_test
+endif
 
 ! since our k2 has zero at Nyquist, we have to do this by hand
-   G_test(lh,:) = 0._rprec
-   G_test(:,ny/2+1) = 0._rprec
+G_test(lh,:) = 0._rprec
+G_test(:,ny/2+1) = 0._rprec
 
-
-! Second test filter, if necessary
-if ((sgs_model == 3) .or. (sgs_model == 5)) then  !--scale dependent dynamic
-
+! Second test filter, if necessary (with scale dependent dynamic)
+if ((sgs_model == 3) .or. (sgs_model == 5)) then
     ! Allocate the arrays
-        allocate ( G_test_test(lh,ny) )
+    allocate ( G_test_test(lh,ny) )
 
     ! Include the normalization
-        G_test_test = 1._rprec/(nx*ny)    
+    G_test_test = 1._rprec/(nx*ny)    
 
     ! Filter characteristic width
-        delta_test_test = alpha_test_test * sqrt(dx*dy)
+    delta_test_test = alpha_test_test * sqrt(dx*dy)
 
     ! Calculate the kernel
-        if(ifilter==1) then ! spectral cutoff filter
-           if (sgs_model==6.OR.sgs_model==7) then
-              print *, 'Use Gaussian or Top-hat filter for mixed models'
-              stop
-           endif
-           kc2_test_test = (pi/(delta_test_test))**2
-           where (real(k2, rprec) >= kc2_test_test) G_test_test = 0._rprec
-        else if(ifilter==2) then ! Gaussian filter
-           G_test_test=exp(-(delta_test_test)**2*k2/(4._rprec*6._rprec))*G_test_test  
-        else if(ifilter==3) then ! Top-hat (Box) filter
-           G_test_test= (sin(kx*delta_test_test/2._rprec)*sin(ky*delta_test_test/2._rprec)+1E-8)/&
-                (kx*delta_test_test/2._rprec*ky*delta_test_test/2._rprec+1E-8)*G_test_test
+    ! spectral cutoff filter
+    if (ifilter==1) then 
+        if (sgs_model==6.OR.sgs_model==7) then
+            print *, 'Use Gaussian or Top-hat filter for mixed models'
+            stop
         endif
+        
+        kc2_test_test = (pi/(delta_test_test))**2
+        where (real(k2, rprec) >= kc2_test_test) G_test_test = 0._rprec
+        
+    ! Gaussian filter
+    else if(ifilter==2) then 
+        G_test_test=exp(-(delta_test_test)**2*k2/(4._rprec*6._rprec))          &
+            * G_test_test  
+
+    ! Top-hat (Box) filter
+    else if(ifilter==3) then 
+        G_test_test= (sin(kx*delta_test_test/2._rprec)                         &
+            * sin(ky*delta_test_test/2._rprec)+1E-8)                           &
+            / (kx*delta_test_test/2._rprec*ky*delta_test_test/2._rprec+1E-8)   &
+            * G_test_test
+    endif
 
     ! since our k2 has zero at Nyquist, we have to do this by hand
-       G_test_test(lh,:) = 0._rprec
-       G_test_test(:,ny/2+1) = 0._rprec
+    G_test_test(lh,:) = 0._rprec
+    G_test_test(:,ny/2+1) = 0._rprec
 
 endif
 
-return   
 end subroutine test_filter_init
-!**********************************************************************
 
-!**********************************************************************
-subroutine test_filter ( f )
-!**********************************************************************
+!*******************************************************************************
+subroutine test_filter(f)
+!*******************************************************************************
 ! note: this filters in-place, so input is ruined
-use types,only:rprec
+use types, only : rprec
 use fft
-use param,only:ny
+use param, only : ny
 use emul_complex, only : OPERATOR(.MULR.)
 implicit none
 
-  real(rprec), dimension(:,:), intent(inout) :: f
+real(rprec), dimension(:,:), intent(inout) :: f
 
 !  Perform in-place FFT
-  call dfftw_execute_dft_r2c(forw,f,f)
+call dfftw_execute_dft_r2c(forw, f, f)
 
 ! Perform f = G_test*f, emulating f as complex
 ! Nyquist frequency and normalization is taken care of with G_test
-  f = f .MULR. G_test
+f = f .MULR. G_test
 
-   call dfftw_execute_dft_c2r(back,f,f)     
+call dfftw_execute_dft_c2r(back, f, f)     
 
-return
 end subroutine test_filter
 
-!**********************************************************************
-subroutine test_test_filter ( f )
-!**********************************************************************
+!*******************************************************************************
+subroutine test_test_filter(f)
+!*******************************************************************************
 ! note: this filters in-place, so input is ruined
-use types,only:rprec
+use types, only : rprec
 use fft
-use param,only:ny
+use param, only : ny
 use emul_complex, only : OPERATOR(.MULR.)
 implicit none
 
-  real(rprec), dimension(:,:), intent(inout) :: f
+real(rprec), dimension(:,:), intent(inout) :: f
 
 !  Perform in-place FFT
-  call dfftw_execute_dft_r2c(forw,f,f)
+call dfftw_execute_dft_r2c(forw, f, f)
 
 ! Perform f = G_test*f, emulating f as complex
 ! Nyquist frequency and normalization is taken care of with G_test_test
-  f = f .MULR. G_test_test
+f = f .MULR. G_test_test
 
-   call dfftw_execute_dft_c2r(back,f,f)               
+call dfftw_execute_dft_c2r(back, f, f)               
 
-return
 end subroutine test_test_filter
 
 end module test_filtermodule
