cd444e95ba1d803f0392add9f6295027ce6f56ca
Carl Shapiro <crshapiro@gmail.com>
2017-07-21 12:29:39 -0400

    cleanup  emul_complex.f90

--- emul_complex.f90
+++ emul_complex.f90
@@ -1,5 +1,5 @@
 !!
-!!  Copyright (C) 2011-2013  Johns Hopkins University
+!!  Copyright (C) 2011-2017  Johns Hopkins University
 !!
 !!  This file is part of lesgo.
 !!
@@ -17,16 +17,13 @@
 !!  along with lesgo.  If not, see <http://www.gnu.org/licenses/>.
 !!
 
-!*********************************************************************
+!*******************************************************************************
 module emul_complex
-!*********************************************************************
-! 
+!*******************************************************************************
+!
 ! The purpose of this module is to provide methods for performing
 ! complex operations against real arrays emulating complex arrays.  The
 ! real array is to contain interleaved complex information.
-!  
-! Written by : 
-!     Jason Graham <jgraha8@gmail.com>
 !
 
 use types, only : rprec
@@ -34,52 +31,38 @@ use messages, only : error
 implicit none
 
 save
-private 
+private
 
-! public :: mult_real_complex, &
-!      mult_real_complex_imag, &
-!      mult_real_complex_real 
-
-public :: operator( .MUL. ), &
-     operator( .MULI. ), &
-     operator( .MULR. )
-
-!///////////////////////////////////////
-!/// OPERATORS                       ///
-!///////////////////////////////////////
+public :: operator( .MUL. ), operator( .MULI. ), operator( .MULR. )
 
 ! REAL X COMPLEX
-interface operator (.MUL.) 
-   module procedure &
-        mul_real_complex_2D
+interface operator (.MUL.)
+    module procedure mul_real_complex_2D
 end interface
 
 ! REAL X IMAG(COMPLEX)
-interface operator (.MULI.) 
-   module procedure &
-        mul_real_complex_imag_scalar, &
-        mul_real_complex_imag_2D 
+interface operator (.MULI.)
+    module procedure mul_real_complex_imag_scalar, mul_real_complex_imag_2D
 end interface
 
 ! REAL X REAL(COMPLEX)
 interface operator (.MULR.)
-   module procedure &
-        mul_real_complex_real_2D
+    module procedure mul_real_complex_real_2D
 end interface
 
 contains
 
-!+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+!*******************************************************************************
 function mul_real_complex_imag_scalar( a, a_c ) result(b)
-!+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+!*******************************************************************************
 !
 !  This function emulates the multiplication of two complex scalars
-!  by emulating the input real vector (a) as a complex type. This 
+!  by emulating the input real vector (a) as a complex type. This
 !  subroutine ignores the real part of a_c (e.g. would use this when
 !  real(a_c) = 0).
 !
 !  Input:
-!  
+!
 !    a (real,size(2,1))  - input real vector
 !    a_c (real)          - input imaginary part of complex scalar
 !
@@ -96,7 +79,7 @@ real(rprec), dimension(2) :: b
 
 !  Cached variables
 real(rprec) :: a_c_i
-  
+
 !  Cache multi-usage variables
 a_c_i = a_c
 
@@ -104,27 +87,25 @@ a_c_i = a_c
 b(1) = - a(2) * a_c_i
 b(2) =  a(1) * a_c_i
 
-return
-
 end function mul_real_complex_imag_scalar
 
-!+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+!*******************************************************************************
 function mul_real_complex_2D( a, a_c ) result(b)
-!+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+!*******************************************************************************
 !  This function emulates the multiplication of two complex 2D array by
 !  emulating the input real array (a) as a complex type.
 !
 !  Input:
-!  
+!
 !    a (real,size(nx_r,ny))     - input real array
-!    a_c (complex,size(nx_c,ny))- input complex array 
+!    a_c (complex,size(nx_c,ny))- input complex array
 !
 !  Output:
-! 
+!
 !    b (real,size(nx_r,ny))     - output real array
 !
 !  Note: nx_c must be nx_r/2
-!  
+!
 implicit none
 
 real(rprec), dimension( :, :), intent(in) :: a
@@ -149,45 +130,40 @@ allocate( b(nx_r, ny) )
 
 !  Emulate complex multiplication
 !  Using outer loop to get contiguous memory access
-do j=1, ny
-  do i=1,nx_c
-
+do j = 1, ny
+do i = 1, nx_c
     !  Real and imaginary indicies of a
     ii = 2*i
     ir = ii-1
-  
+
     !  Cache multi-usage variables
     a_r = a(ir,j)
     a_i = a(ii,j)
     a_c_r = real(a_c(i,j),kind=rprec)
     a_c_i = dimag(a_c(i,j))
-    
+
     !  Perform multiplication
     b(ir,j) = a_r * a_c_r - a_i * a_c_i
     b(ii,j) = a_r * a_c_i + a_i * a_c_r
-
-  enddo
 enddo
-
-return
+enddo
 
 end function mul_real_complex_2D
 
-
-!+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+!*******************************************************************************
 function mul_real_complex_imag_2D( a, a_c ) result(b)
-!+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+!*******************************************************************************
 !  This function emulates the multiplication of two complex 2D array by
 !  emulating the input real array (a) as a complex type. This subroutine
 !  ignores the real part of a_c (e.g. would use this when real(a_c) = 0)
 !
 !  Input:
-!  
+!
 !    a (real,size(nx_r,ny))     - input real array
-!    a_c (real,size(nx_c,ny))   - input imaginary part of complex array 
+!    a_c (real,size(nx_c,ny))   - input imaginary part of complex array
 !
 !  Output:
-! 
+!
 !    b (real,size(nx_r,ny))     - output real array
 !
 !  Note: nx_c must be nx_r/2
@@ -207,7 +183,7 @@ integer :: nx_r, nx_c, ny
 
 ! Get the size of the incoming arrays
 nx_r = size(a,1)
-ny   = size(a,2)
+ny = size(a,2)
 
 nx_c = size(a_c,1)
 
@@ -215,13 +191,12 @@ nx_c = size(a_c,1)
 allocate( b(nx_r, ny ) )
 
 !  Emulate complex multiplication
-do j=1, ny !  Using outer loop to get contiguous memory access
-  do i=1,nx_c
-
+do j = 1, ny !  Using outer loop to get contiguous memory access
+do i = 1,nx_c
     !  Real and imaginary indicies of a
     ii = 2*i
     ir = ii-1
-  
+
     !  Cache multi-usage variables
     a_c_i = a_c(i,j)
 
@@ -229,27 +204,23 @@ do j=1, ny !  Using outer loop to get contiguous memory access
     cache = a(ir,j) * a_c_i
     b(ir,j) = - a(ii,j) * a_c_i
     b(ii,j) =  cache
-
-  enddo
 enddo
-
-return
+enddo
 
 end function mul_real_complex_imag_2D
 
-
-!+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+!*******************************************************************************
 function mul_real_complex_real_2D( a, a_c ) result(b)
-!+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+!*******************************************************************************
 !  This function emulates the multiplication of two complex 2D array by
 !  emulating the input real array (a) as a complex type. This subroutine
 !  ignores the imaginary part of a_c (e.g. would use this when imag(a_c)
 !  = 0).
 !
 !  Input:
-!  
+!
 !    a (real,size(nx_r,ny))     - input/output real array
-!    a_c (real,size(nx_c,ny))   - input real part of complex array 
+!    a_c (real,size(nx_c,ny))   - input real part of complex array
 !
 !  Output:
 !
@@ -277,20 +248,16 @@ nx_c = size(a_c,1)
 allocate(b(nx_r,ny))
 
 !  Emulate complex multiplication
-do j=1, ny !  Using outer loop to get contiguous memory access
-  do i=1,nx_c
-
+do j = 1, ny !  Using outer loop to get contiguous memory access
+do i = 1, nx_c
     !  Real and imaginary indicies of a
     ii = 2*i
     ir = ii-1
 
     !  Perform multiplication
     b(ir:ii,j) = a_c(i,j)*a(ir:ii,j)
-
-  enddo
 enddo
-
-return
+enddo
 
 end function mul_real_complex_real_2D
 
